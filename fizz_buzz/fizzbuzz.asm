c4d3e294ec777dd8555c2251863ffd5cbf1e7d79
jwl-7 <lucidrainbows@gmail.com>
2018-11-03 00:06:12 -0400

    added console output

--- fizz_buzz/fizzbuzz.asm
+++ fizz_buzz/fizzbuzz.asm
@@ -1,8 +1,6 @@
-TITLE FizzBuzz
-
-;--------------------
-;     Fizz Buzz     ;
-;--------------------
+;=====================
+;=     Fizz Buzz     =
+;=====================
 
 ; This is written in x86 assembly for MASM.
 ; This program prints each number from 1 to 100 on a new line.
@@ -10,25 +8,49 @@ TITLE FizzBuzz
 ; For each multiple of 5, it prints 'Buzz' instead of the number.
 ; For numbers which are multiples of both 3 and 5, it prints 'FizzBuzz' instead of the number.
 
+TITLE FizzBuzz
+
 .386
 .MODEL flat, stdcall
 .STACK 4096
-ExitProcess PROTO,dwExitCode:DWORD
+
+ExitProcess PROTO, dwExitCode:DWORD			; ends process and its threads
+STD_OUTPUT_HANDLE EQU -11					; standard output device
+WriteConsole EQU <WriteConsoleA>
+GetStdHandle PROTO,               			; get standard handle
+	nStdHandle:DWORD  						; type of console handle
+WriteConsole PROTO,							; write a buffer to the console
+    handle:DWORD,							; output handle
+    lpBuffer:PTR BYTE,						; pointer to buffer
+    nNumberOfCharsToWrite:DWORD,			; size of buffer
+    lpNumberOfCharsWritten:PTR DWORD,		; number of chars written
+    lpReserved:PTR DWORD					; 0 (not used)
 
 .DATA
-    fizz BYTE 'Fizz', 0             ; 'Fizz' string
-    buzz BYTE 'Buzz', 0             ; 'Buzz' string
-    fizzbuzz BYTE 'FizzBuzz', 0     ; 'FizzBuzz' string
+    fizz BYTE 'Fizz', 0
+    buzz BYTE 'Buzz', 0
+    fizzbuzz BYTE 'FizzBuzz', 0
+
+.DATA?
+	consoleOutHandle dword ?
+	bytesWritten dword ?
 
 .CODE
 main PROC
-    mov ecx, 100
+    ;mov ecx, 100							; set counter to 100
+	INVOKE GetStdHandle,					; get standard handle 
+		STD_OUTPUT_HANDLE					; standard output device
+	mov consoleOutHandle, eax				; EAX = consoleOutHandle
+	INVOKE WriteConsole,					; write buffer to console
+		consoleOutHandle,					; output handle	
+		OFFSET fizz,						; address of fizz
+		LENGTHOF fizz - 1,					; length of fizz
+		OFFSET bytesWritten, 0				; address of bytesWriten
 
-fizzy:
+;fizzy:
 
+    ;loop fizzy            
 
-    loop fizzy            
-
-    INVOKE ExitProcess, 0
+    INVOKE ExitProcess, 0	; exit code = 0
 main ENDP
 END main
