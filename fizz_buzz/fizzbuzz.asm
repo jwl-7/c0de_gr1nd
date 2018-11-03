fc70fa8d231a5056cfc611b58f03ecddeb0c9a8b
jwl-7 <lucidrainbows@gmail.com>
2018-11-03 04:59:32 -0400

    added StrLength procedure

--- fizz_buzz/fizzbuzz.asm
+++ fizz_buzz/fizzbuzz.asm
@@ -147,4 +147,24 @@ PrintStr PROC
     ret
 PrintStr ENDP
 
+;====================================================================
+;=                            StrLength                             =
+;====================================================================
+;- Gets length of a null-terminated string.                         -
+;- Receives: pString -> string pointer                              -
+;- Returns: EAX = string length                                     -
+;--------------------------------------------------------------------
+StrLength PROC USES edi,
+	pString:PTR BYTE
+	mov edi,pString
+	mov eax,0     	                ; character count
+L1:
+	cmp BYTE PTR [edi],0	      ; end of string?
+	je  L2	                     ; yes: quit
+	inc edi	                     ; no: point to next
+	inc eax	                     ; add 1 to count
+	jmp L1
+L2: ret
+StrLength ENDP
+
 END main
