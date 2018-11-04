fe549ecbf58723b4b437fcca76b3369dc376df71
jwl-7 <lucidrainbows@gmail.com>
2018-11-04 17:01:44 -0500

    init commit

---
+++ balanced_brackets/balanced-brackets.py
@@ -0,0 +1,25 @@
+class Solution:
+    def balanced_brackets(self, expression):
+        opening = tuple('[({')
+        closing = tuple('])}')
+        brackets = dict(zip(opening, closing))
+        stack = []
+
+        for char in expression:
+            if char in opening:
+                stack.append(char)
+            elif stack and char in closing:
+                stack.pop()
+        print(stack)
+        if not stack:
+            print('> BALANCED')
+        else:
+            print('> NOT BALANCED')
+    
+def main():
+    sol = Solution()
+    print('Enter expression: ')
+    statement = input()
+    sol.balanced_brackets(statement)
+
+main()
