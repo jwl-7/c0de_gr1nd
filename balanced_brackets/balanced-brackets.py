0dccab87d48ddccfdbb60abf22c217db1f54020e
jwl-7 <lucidrainbows@gmail.com>
2018-11-04 22:48:54 -0500

    fixed algorithm and added test cases

--- balanced_brackets/balanced-brackets.py
+++ balanced_brackets/balanced-brackets.py
@@ -16,19 +16,22 @@ class Solution:
         for char in expression:
             if char in opening:
                 stack.append(char)
-                peek = stack[-1]
             elif not stack and char in closing:
                 return False     
-            elif stack and char in closing and char == brackets[peek]:
-                stack.pop()
+            elif stack:
+                peek = stack[-1]
+                if char in closing and char == brackets[peek]:
+                    stack.pop()
         return not stack
     
 def main():
     s = Solution()
 
     #print('Enter expression: ')
-    #statement = input()
-    expression = 'test() 123()'
-    print(s.is_balanced(expression))
+    #expression = input()
+    #print(s.is_balanced(expression))
+    print(s.is_balanced('(([])){[()][]}'))
+    print(s.is_balanced('())[]{}'))
+    print(s.is_balanced('[(])'))
 
 main()
