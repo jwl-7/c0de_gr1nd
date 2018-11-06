8ad7bbd552daa19f4dd4660ccf60975ee9177b42
jwl-7 <lucidrainbows@gmail.com>
2018-11-06 00:10:52 -0500

    improved algorithm slightly

--- fizz_buzz/fizzbuzz.py
+++ fizz_buzz/fizzbuzz.py
@@ -9,7 +9,7 @@
 
 def fizzy():
     for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
+        if i % 15 == 0:
             print('FizzBuzz')
         elif i % 3 == 0:
             print('Fizz')
