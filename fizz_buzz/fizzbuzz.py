3f7aa908efeca7b7fd5806a9650ba42becb466b0
jwl-7 <lucidrainbows@gmail.com>
2018-11-09 16:04:55 -0500

    changed 'num' to 'n'

--- fizz_buzz/fizzbuzz.py
+++ fizz_buzz/fizzbuzz.py
@@ -11,9 +11,9 @@
 # Space Complexity: O(n)
 
 class Solution:
-    def fizzy(self, num):
+    def fizzy(self, n):
         list = []
-        for i in range(1, num + 1):
+        for i in range(1, n + 1):
             if i % 15 == 0: list.append('FizzBuzz')
             elif i % 3 == 0: list.append('Fizz')
             elif i % 5 == 0: list.append('Buzz')
