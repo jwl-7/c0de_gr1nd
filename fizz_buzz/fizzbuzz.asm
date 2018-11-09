a0636b4555441e6379b2c4d823dc9c9961dbd2ce
jwl-7 <lucidrainbows@gmail.com>
2018-11-09 02:39:56 -0500

    analyzed time and space complexity

--- fizz_buzz/fizzbuzz.asm
+++ fizz_buzz/fizzbuzz.asm
@@ -2,13 +2,14 @@
 ;=                            FIZZ BUZZ                             =                    
 ;====================================================================
 
-; This is written in x86 Assembly for MASM.
+; This is written in x86 Assembly (MASM).
 
 ; Problem: Write a short program that prints each number from 1 to 100 on a new line. 
 ;          For each multiple of 3, print 'Fizz' instead of the number. 
 ;          For each multiple of 5, print 'Buzz' instead of the number. 
 ;          For numbers which are multiples of both 3 and 5, print 'FizzBuzz' instead of the number.
-; Runtime:
+;  Time Complexity: O(n)
+; Space Complexity: O(n)
 
 TITLE FizzBuzz
 
--- fizz_buzz/fizzbuzz.py
+++ fizz_buzz/fizzbuzz.py
@@ -6,8 +6,8 @@
 #          For each multiple of 3, print 'Fizz' instead of the number. 
 #          For each multiple of 5, print 'Buzz' instead of the number. 
 #          For numbers which are multiples of both 3 and 5, print 'FizzBuzz' instead of the number.
-#  Time Complexity:
-# Space Complexity:
+#  Time Complexity: O(n)
+# Space Complexity: O(n)
 
 class Solution:
     def fizzy(self):
