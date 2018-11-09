9d9101688c7504274215db8e8832748dc8a6f583
jwl-7 <lucidrainbows@gmail.com>
2018-11-09 00:45:20 -0500

    added stylish title header and revised comments

--- fizz_buzz/fizzbuzz.py
+++ fizz_buzz/fizzbuzz.py
@@ -1,11 +1,12 @@
-#####################
-#     Fizz Buzz     #
-#####################
+#====================================================================
+#=                            FIZZ BUZZ                             =                    
+#====================================================================
 
-# This program prints each number from 1 to 100 on a new line.
-# For each multiple of 3, it prints 'Fizz' instead of the number.
-# For each multiple of 5, it prints 'Buzz' instead of the number.
-# For numbers which are multiples of both 3 and 5, it prints 'FizzBuzz' instead of the number.
+# Problem: Write a short program that prints each number from 1 to 100 on a new line. 
+#          For each multiple of 3, print 'Fizz' instead of the number. 
+#          For each multiple of 5, print 'Buzz' instead of the number. 
+#          For numbers which are multiples of both 3 and 5, print 'FizzBuzz' instead of the number.
+# Runtime: 
 
 def fizzy():
     for i in range(1, 101):
