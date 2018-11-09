e2c6e5492b64d975e36f0060d8831aa926a7150d
jwl-7 <lucidrainbows@gmail.com>
2018-11-09 00:48:33 -0500

    stylized header comments

--- fizz_buzz/fizzbuzz.asm
+++ fizz_buzz/fizzbuzz.asm
@@ -3,10 +3,12 @@
 ;====================================================================
 
 ; This is written in x86 Assembly for MASM.
-; This program prints each number from 1 to 100 on a new line.
-; For each multiple of 3, it prints 'Fizz' instead of the number.
-; For each multiple of 5, it prints 'Buzz' instead of the number.
-; For numbers which are multiples of both 3 and 5, it prints 'FizzBuzz' instead of the number.
+
+; Problem: Write a short program that prints each number from 1 to 100 on a new line. 
+;          For each multiple of 3, print 'Fizz' instead of the number. 
+;          For each multiple of 5, print 'Buzz' instead of the number. 
+;          For numbers which are multiples of both 3 and 5, print 'FizzBuzz' instead of the number.
+; Runtime:
 
 TITLE FizzBuzz
 
