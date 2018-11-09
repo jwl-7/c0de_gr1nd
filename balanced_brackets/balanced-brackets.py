64522f4b1e33baf0415f395a4759d89bb5898c7c
jwl-7 <lucidrainbows@gmail.com>
2018-11-09 03:16:57 -0500

    added stylish comment header

--- balanced_brackets/balanced-brackets.py
+++ balanced_brackets/balanced-brackets.py
@@ -1,10 +1,18 @@
-#=====================
-#= Balanced Brackets =
-#=====================
+#====================================================================
+#=                        Valid Parentheses                         =
+#====================================================================
 
-# Problem: Given an expression string, determine whether or not
-#          each sequence of '[](){}' is balanced.
-# Runtime: O(n)
+# Problem: Given a string containing just the characters 
+#          '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+#
+#          An input string is valid if:
+#              Open brackets must be closed by the same type of brackets.
+#              Open brackets must be closed in the correct order.
+#
+#          Note that an empty string is also considered valid.
+#
+#  Time Complexity: O(n)
+# Space Complexity: O(n)
 
 class Solution:
     def is_balanced(self, expression):
