5d1cc50346a5cf1f75d02647fe1f24eb2be99057
jwl-7 <lucidrainbows@gmail.com>
2018-11-09 16:43:57 -0500

    changed problem to leetcode style question

--- fizz_buzz/fizzbuzz.asm
+++ fizz_buzz/fizzbuzz.asm
@@ -4,7 +4,7 @@
 
 ; This is written in x86 Assembly (MASM).
 ;
-; Problem: Write a short program that prints each number from 1 to 100 on a new line. 
+; Problem: Write a program that outputs the string representation of numbers from 1 to n. 
 ;          For each multiple of 3, print 'Fizz' instead of the number. 
 ;          For each multiple of 5, print 'Buzz' instead of the number. 
 ;          For numbers which are multiples of both 3 and 5, print 'FizzBuzz' instead of the number.
