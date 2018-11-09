a3f3fc663bc341f3937595cf1d93f71bc9f9c629
jwl-7 <lucidrainbows@gmail.com>
2018-11-09 14:46:06 -0500

    adjusted comment header

--- fizz_buzz/fizzbuzz.py
+++ fizz_buzz/fizzbuzz.py
@@ -6,6 +6,7 @@
 #          For each multiple of 3, print 'Fizz' instead of the number. 
 #          For each multiple of 5, print 'Buzz' instead of the number. 
 #          For numbers which are multiples of both 3 and 5, print 'FizzBuzz' instead of the number.
+#
 #  Time Complexity: O(n)
 # Space Complexity: O(n)
 
