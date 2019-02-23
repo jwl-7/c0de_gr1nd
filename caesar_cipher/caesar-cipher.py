9fd986c7ad7561e416b4cc4ac10dc0994ef1b9e6
jwl-7 <lucidrainbows@gmail.com>
2019-02-23 01:13:23 -0500

    init commit

---
+++ caesar_cipher/caesar-cipher.py
@@ -0,0 +1,17 @@
+#====================================================================
+#=                          CAESAR CIPHER                           =                    
+#====================================================================
+
+# Problem: Write a program that decrypts ciphertext that has been encrypted
+#          using the Caesar Cipher. Find the plaintext and the shift key.
+
+class Solution:
+    def get_ciphertext(self):
+        ciphertext = input('Enter ciphertext: ')
+
+def main():
+    s = Solution()
+    s.get_ciphertext()
+
+if __name__ == "__main__":
+    main()
