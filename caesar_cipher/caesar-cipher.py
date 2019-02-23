fea5586877a06f73977722daf40c5f188aae2044
jwl-7 <lucidrainbows@gmail.com>
2019-02-23 02:27:28 -0500

    added skeleton of program

--- caesar_cipher/caesar-cipher.py
+++ caesar_cipher/caesar-cipher.py
@@ -4,14 +4,31 @@
 
 # Problem: Write a program that decrypts ciphertext that has been encrypted
 #          using the Caesar Cipher. Find the plaintext and the shift key.
+import enchant
 
 class Solution:
-    def get_ciphertext(self):
+    def cipher_key(self, string):
+        alphabet = tuple('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+        alphabet_numbers = []
+        char_frequency = {}
+
+        for char in string:
+            keys = char_frequency.keys()
+
+            if char in keys:
+                char_frequency[char] += 1
+            else:
+                char_frequency[char] = 1
+
+        
+    def detect_word(self):
+        d = enchant.Dict('en_US')
+
+    def decrypt(self):
         ciphertext = input('Enter ciphertext: ')
 
 def main():
     s = Solution()
-    s.get_ciphertext()
 
 if __name__ == "__main__":
     main()
