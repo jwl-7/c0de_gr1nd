142c1605a11677d29a04e9f8d5d5f8dbd15e5b26
jwl-7 <lucidrainbows@gmail.com>
2019-02-24 23:03:42 -0500

    trimmed non-working code

--- caesar_cipher/caesar-cipher.py
+++ caesar_cipher/caesar-cipher.py
@@ -4,40 +4,36 @@
 
 # Problem: Write a program that decrypts ciphertext that has been encrypted
 #          using the Caesar Cipher. Find the plaintext and the shift key.
+
 import enchant
 
 class Solution:
     def check_frequency(self, string):
-        alphabet = tuple('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-        alphabet_nums = []
         char_freq = {}
 
         for char in string:
-            keys = char_freq.keys()
-
-            if char in keys:
+            freq = char_freq.keys()
+            if char in freq:
                 char_freq[char] += 1
             else:
                 char_freq[char] = 1
+        char_freq = sorted(char_freq.items(), key = lambda x: x[1], reverse = True)
+        return char_freq[:4]
 
-        sorted_char_freq = sorted(char_freq.items(), key = lambda x: x[1], reverse = True)
-
-        return sorted_char_freq[:4]
-
-    def cipher_key(self, string):
-        sdalphabet = tuple('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-  
-    def detect_word(self):
+    def check_word(self, string):
         d = enchant.Dict('en_US')
+        return d.check(string)
 
     def decrypt(self):
-        ciphertext = input('Enter ciphertext: ')
+        #ciphertext = str.upper(input('Enter ciphertext: '))
+        ciphertext = str.upper('OTWEWNGWCBPQABIZVQAPMLJGZWTTQVOBQUMAPMIDGZCAB')
         top_letters = self.check_frequency(ciphertext)
 
         print('Top Four Letters')
         print('-----------------')
         for item in top_letters:
-            print(item[0], ' : ', item[1])
+            print(str.upper(item[0]), ' : ', item[1])
+        
 
 def main():
     s = Solution()
