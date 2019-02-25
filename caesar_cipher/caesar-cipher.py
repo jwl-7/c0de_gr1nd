27e48a55891e8fdf4639b71bf84d78cda6dbae4a
jwl-7 <lucidrainbows@gmail.com>
2019-02-24 23:34:25 -0500

    removed code that required enchant pkg

--- caesar_cipher/caesar-cipher.py
+++ caesar_cipher/caesar-cipher.py
@@ -5,8 +5,6 @@
 # Problem: Write a program that decrypts ciphertext that has been encrypted
 #          using the Caesar Cipher. Find the plaintext and the shift key.
 
-import enchant
-
 class Solution:
     def check_frequency(self, string):
         char_freq = {}
@@ -18,15 +16,13 @@ class Solution:
             else:
                 char_freq[char] = 1
         char_freq = sorted(char_freq.items(), key = lambda x: x[1], reverse = True)
-        return char_freq[:4]
 
-    def check_word(self, string):
-        dictionary = enchant.Dict('en_US')
-        return dictionary.check(string)
+        return char_freq[:4]
     
     def decrypt(self, string, key):
         decrypted = ''
         key =- key
+
         for char in string:
             num = ord(char)
             num += key
@@ -35,8 +31,8 @@ class Solution:
                 num -= 26
             elif num < ord('A'):
                 num += 26
-
             decrypted += chr(num)
+
         return decrypted
 
     def display(self):
@@ -44,10 +40,22 @@ class Solution:
         ciphertext = str.upper('OTWEWNGWCBPQABIZVQAPMLJGZWTTQVOBQUMAPMIDGZCAB')
         top_letters = self.check_frequency(ciphertext)
 
+        p_keys = []
+
         print('Top Four Letters')
         print('-----------------')
         for item in top_letters:
             print(str.upper(item[0]), ' : ', item[1])
+
+            num = ord(item[0])
+            if num > ord('Z'):
+                num -= 26
+            elif num < ord('A'):
+                num += 26
+            print(ord(item[0]))
+
+
+            
         
         print(self.decrypt(ciphertext, 8))
 
