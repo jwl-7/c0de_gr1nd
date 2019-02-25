cddad9e10384ccc4ded039e949cba8ff55d08831
jwl-7 <lucidrainbows@gmail.com>
2019-02-25 03:38:14 -0500

    changed display to cipher_run function and implemented modes

--- caesar_cipher/caesar-cipher.py
+++ caesar_cipher/caesar-cipher.py
@@ -26,21 +26,6 @@ class Cipher:
                 print('[ERROR] INVALID INPUT!')
 
         return mode
-        
-    def check_frequency(self, msg):
-        """Finds the top four most frequently used letters in the ciphertext."""
-
-        char_freq = {}
-
-        for char in msg:
-            freq = char_freq.keys()
-            if char in freq:
-                char_freq[char] += 1
-            else:
-                char_freq[char] = 1
-        char_freq = sorted(char_freq.items(), key = lambda x: x[1], reverse = True)
-
-        return char_freq[:4]
     
     def transcipher(self, mode, msg, key):
         """Shifts each letter in the ciphertext n positions to the right."""
@@ -66,6 +51,21 @@ class Cipher:
 
         return translation
 
+    def find_frequency(self, msg):
+        """Finds the top four most frequently used letters in the ciphertext."""
+
+        char_freq = {}
+
+        for char in msg:
+            freq = char_freq.keys()
+            if char in freq:
+                char_freq[char] += 1
+            else:
+                char_freq[char] = 1
+        char_freq = sorted(char_freq.items(), key = lambda x: x[1], reverse = True)
+
+        return char_freq[:4]
+
     def cipher_key(self, msg, top_char):
         """Finds possible cipher keys using most frequently used letters."""
 
@@ -81,31 +81,36 @@ class Cipher:
         return p_keys
 
 
-    def display(self):
+    def cipher_run(self):
         """Runs the Caesar cipher decryption / frequency analysis."""
 
         cipher_mode = self.prompt()
 
-        ciphertext = str.upper(input('Enter ciphertext: '))
-        top_letters = self.check_frequency(ciphertext)
-        possible_keys = self.cipher_key(ciphertext, top_letters)
+        if cipher_mode == 1:
+            ciphertext = str.upper(input('Enter plaintext: '))
+        elif cipher_mode == 2:
+            ciphertext = str.upper(input('Enter ciphertext: '))
+            top_letters = self.find_frequency(ciphertext)
+            possible_keys = self.cipher_key(ciphertext, top_letters)
 
-        print('Top Four Letters')
-        print('-------------------')
-        for item in top_letters:
-            print(f'{item[0]} : {item[1]}')
+            print('Top Four Letters')
+            print('-------------------')
+            for item in top_letters:
+                print(f'{item[0]} : {item[1]}')
 
-        print(f'\nPossible Shift Keys: {possible_keys}')
+            print(f'\nPossible Shift Keys: {possible_keys}')
 
-        print('\nPossible Translations')
-        print('-----------------------')
-        for key in possible_keys:
-            p_msg = self.decrypt(ciphertext, key)
-            print(f'[{key}] {p_msg} \n')
+            print('\nPossible Translations')
+            print('-----------------------')
+            for key in possible_keys:
+                p_msg = self.decrypt(ciphertext, key)
+                print(f'[{key}] {p_msg} \n')
+        elif cipher_mode == 3:
+            ciphertext = str.upper(input('Enter ciphertext: '))
 
 def main():
     c = Cipher()
-    c.display()
+    c.cipher_run()
 
 if __name__ == "__main__":
     main()
