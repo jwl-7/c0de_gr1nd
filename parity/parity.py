dd7ded37a4e596b3c2e20c6b0827c9e059c403a3
jwl-7 <lucidrainbows@gmail.com>
2019-05-19 23:23:19 -0400

    added skeleton and first solution

---
+++ parity/parity.py
@@ -0,0 +1,22 @@
+class Solution:
+    def parity(self, x):
+        result = 0
+        while x:
+            result ^= x & 1
+            x >>= 1
+        return result
+    
+def main():
+    s = Solution()
+    test_cases = [ # these are not actually words - used for proof of concept
+        0b1011,
+        0b1001,
+        0b100100,
+        0b1110000
+        ]
+    for num in test_cases:
+        result = s.parity(num)
+        print(f'{bin(num)[2:]} -> {result}')
+
+if __name__ == '__main__':
+    main()
