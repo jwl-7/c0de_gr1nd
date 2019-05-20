b09ef84c1e48bd2c1cad061e85d4a2a59eaaafef
jwl-7 <lucidrainbows@gmail.com>
2019-05-20 00:49:47 -0400

    added my sol

---
+++ parity/parity-mysol.py
@@ -0,0 +1,27 @@
+"""Brute-Force Algorithm
+
+Time Complexity = O(n), where n is the word size
+"""
+
+class Solution:
+    def parity(self, x):
+        result = 0
+        while x:
+            result = not result
+            x = x & (x - 1)
+        return int(result)
+    
+def main():
+    s = Solution()
+    test_cases = [
+        0b1011,
+        0b1001,
+        0b100100,
+        0b1110000
+        ]
+    for num in test_cases:
+        result = s.parity(num)
+        print(f'{bin(num)[2:]} -> {result}')
+
+if __name__ == '__main__':
+    main()
