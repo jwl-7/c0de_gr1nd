05edef2c685878dad3e07e9a1bd7c9fdae447fb9
jwl-7 <lucidrainbows@gmail.com>
2019-05-20 03:00:18 -0400

    added my sol

--- swap_bits/swapbits-mysol.py
+++ swap_bits/swapbits-mysol.py
@@ -5,11 +5,18 @@ Time Complexity = O(1), independent of the word size
 
 class Solution:
     def swap_bits(self, x, i, j):
+        pos_i = x >> i & 1
+        pos_j = x >> j & 1
+        if pos_i != pos_j:
+            a = 1 << i
+            b = 1 << j
+            x ^= a | b
         return x
     
 def main():
     s = Solution()
     test_cases = [
+        1234,
         2198274943350798936,
         1934222162669788191,
         2270450305416888212,
@@ -17,7 +24,7 @@ def main():
         7060433214945544085
         ]
     for num in test_cases:
-        i = 1
+        i = 3
         j = 7
         swapped_number = s.swap_bits(num, i, j)
         print(f'{num} -> {swapped_number}')
