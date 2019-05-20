0e6c1fb9564667e4fa7cf4943a4a63612e3adfcf
jwl-7 <lucidrainbows@gmail.com>
2019-05-20 04:50:37 -0400

    added correct sol

--- reverse_bits/reversebits-mysol.py
+++ reverse_bits/reversebits-mysol.py
@@ -5,8 +5,12 @@ Time Complexity: O(n)
 
 class Solution:
     def reverse_bits(self, x):
-        return x
-    
+        reverse = 0
+        for i in range(64):
+            reverse = (reverse << 1) + (x & 1)
+            x >>= 1
+        return reverse
+        
 def main():
     s = Solution()
     test_cases = [
@@ -16,8 +20,8 @@ def main():
         0b1110011001100000
         ]
     for num in test_cases:
-        reversed_num = s.parity(num)
-        print(f'{bin(num)[2:]} -> {reversed_num}')
+        reversed_num = s.reverse_bits(num)
+        print(f'{bin(num)[2:]} -> {bin(reversed_num)[2:]}')
 
 if __name__ == '__main__':
     main()
