103f3063fedfa4a9df000293e7f9b24094e1e743
jwl-7 <lucidrainbows@gmail.com>
2019-05-20 00:19:05 -0400

    added book sol #2

---
+++ parity/parity-booksol_2.py
@@ -0,0 +1,27 @@
+"""Improved Brute-Force Algorithm
+
+Time Complexity = O(k)
+"""
+
+class Solution:
+    def parity(self, x):
+        result = 0
+        while x:
+            result ^= 1
+            x &= x - 1 # drops the lowest set bit of x
+        return result
+    
+def main():
+    s = Solution()
+    test_cases = [
+        0b1011,
+        0b1001,
+        0b100100,
+        0b1110000
+        ]
+    for num in test_cases:
+        result = s.parity(num)
+        print(f'{bin(num)[2:]} -> {result}')
+
+if __name__ == '__main__':
+    main()
