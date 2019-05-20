8052cc0b60cde22f4098ec5a1cd1c35066fc6eb2
jwl-7 <lucidrainbows@gmail.com>
2019-05-20 03:43:06 -0400

    style cleanup

--- caesar_cipher/caesar-cipher.py
+++ caesar_cipher/caesar-cipher.py
@@ -1,10 +1,9 @@
-#====================================================================
-#=                          CAESAR CIPHER                           =                    
-#====================================================================
+"""Caesar Cipher
 
-# This program allows you to encrypt / decrypt ciphertexts using the Caesar cipher.
-# Plaintext is encrypted using shifts to the right.
-# Ciphertext is decrypted using either basic frequency analysis or brute force.
+This program allows you to encrypt / decrypt ciphertexts using the Caesar cipher.
+Plaintext is encrypted using shifts to the right.
+Ciphertext is decrypted using either basic frequency analysis or brute force.
+"""
 
 class Cipher:
     def prompt(self):
@@ -15,7 +14,6 @@ class Cipher:
         print('1 - Encrypt plaintext -> ciphertext')
         print('2 - Decrypt ciphertext -> plaintext using frequency analysis')
         print('3 - Decrypt ciphertext -> plaintext using brute force\n')
-
         while True:
             try:
                 mode = int(input('Pick a mode [1-3]: '))
@@ -25,31 +23,25 @@ class Cipher:
                 print('[ERROR] INVALID INPUT!')
             else:
                 break
-
         return mode
     
     def transcipher(self, mode, msg, key):
         """Shifts each letter in the ciphertext n positions to the right."""
 
         translation = ''
-
         if mode == 2 or mode == 3:
             key =- key
-
         for char in msg:
             if char.isalpha():
                 num = ord(char)
                 num += key
-
                 if num > ord('Z'):
                     num -= 26
                 elif num < ord('A'):
                     num += 26
-
                 translation += chr(num)
             else:
                 translation += char
-
         return translation
 
     def get_key(self):
@@ -64,14 +56,12 @@ class Cipher:
                 print('[ERROR] INVALID INPUT!')
             else:
                 break
-
         return u_key
 
     def find_frequency(self, msg):
         """Finds the top four most frequently used letters in the ciphertext."""
 
         char_freq = {}
-
         for char in msg:
             freq = char_freq.keys()
             if char.isalpha():
@@ -80,21 +70,18 @@ class Cipher:
                 else:
                     char_freq[char] = 1
         char_freq = sorted(char_freq.items(), key = lambda x: x[1], reverse = True)
-
         return char_freq[:4]
 
     def find_key(self, msg, top_char):
         """Finds top four most likely cipher keys using 'E' as a reference."""
 
         p_keys = []
-
         for char in top_char:
             num = ord(char[0]) - 65
             a = (num - 4) % 26
             b = (4 - num) % 26
             num = min(a, b)
             p_keys.append(num)
-
         return p_keys
 
 
@@ -102,40 +89,31 @@ class Cipher:
         """Runs the Caesar cipher encryption / decryption."""
 
         cipher_mode = self.prompt()
-
         if cipher_mode == 1:
             plaintext = str.upper(input('Enter plaintext: '))
             user_key = self.get_key()
             user_msg = self.transcipher(1, plaintext, user_key)
-
             print('\nEncipherment')
             print('-------------------------------------------')
             print(f'[{user_key}] {user_msg} \n')
-            
         elif cipher_mode == 2:
             ciphertext = str.upper(input('Enter ciphertext: '))
             top_letters = self.find_frequency(ciphertext)
             possible_keys = self.find_key(ciphertext, top_letters)
-
             print('\nFrequency Analysis')
             print('-------------------------------------------')
-
             for letter in top_letters:
                 print(f'{letter[0]} : {letter[1]}')
-
             print(f'\nTop Possible Shift Keys: {possible_keys}')
             print('\nTop Possible Translations')
             print('-------------------------------------------')
-
             for p_key in possible_keys:
                 p_msg = self.transcipher(2, ciphertext, p_key)
                 print(f'[{p_key}] {p_msg}')
         elif cipher_mode == 3:
             ciphertext = str.upper(input('Enter ciphertext: '))
-
             print('\nBrute Force')
             print('-------------------------------------------')
-
             for i in range (1, 27):
                 p_msg = self.transcipher(3, ciphertext, i)
                 print(f'[{i}] {p_msg}')
