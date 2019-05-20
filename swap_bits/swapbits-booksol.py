cc12f4f3b00905cf9e17743d148fc2a958f1613d
jwl-7 <lucidrainbows@gmail.com>
2019-05-20 03:03:11 -0400

    added book sol

---
+++ swap_bits/swapbits-booksol.py
@@ -0,0 +1,30 @@
+"""Brute-Force Algorithm
+
+Time Complexity = O(1), independent of the word size
+"""
+
+class Solution:
+    def swap_bits(self, x, i, j):
+        if (x >> i) & 1 != (x >> j) & 1:
+            bit_mask = (1 << i) | (1 << j)
+            x ^= bit_mask
+        return x
+    
+def main():
+    s = Solution()
+    test_cases = [
+        1234,
+        2198274943350798936,
+        1934222162669788191,
+        2270450305416888212,
+        5820431162416610477,
+        7060433214945544085
+        ]
+    for num in test_cases:
+        i = 3
+        j = 7
+        swapped_number = s.swap_bits(num, i, j)
+        print(f'{num} -> {swapped_number}')
+
+if __name__ == '__main__':
+    main()
