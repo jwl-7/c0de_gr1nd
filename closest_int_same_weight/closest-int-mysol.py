dbc5b24c6b0da412c5204f8ff8fe61ebd9a20ae6
jwl-7 <lucidrainbows@gmail.com>
2019-05-20 20:00:56 -0400

    added solution

--- closest_int_same_weight/closest-int-mysol.py
+++ closest_int_same_weight/closest-int-mysol.py
@@ -4,8 +4,14 @@ Time Complexity: O(n), where n is the integer width
 """
 
 class Solution:
-    def closest(self, x):
-        return x
+    def closest_int_same_bit_count(self, x):
+        for i in range(63):
+            index_i = x >> i & 1
+            index_j = x >> i + 1 & 1
+            if index_i != index_j:
+                x ^= 1 << i
+                x ^= 1 << i + 1
+                return x
         
 def main():
     s = Solution()
@@ -16,7 +22,7 @@ def main():
         74
         ]
     for num in test_cases:
-        closest_int = s.closest(num)
+        closest_int = s.closest_int_same_bit_count(num)
         print(f'{num} -> {closest_int}')
 
 if __name__ == '__main__':
