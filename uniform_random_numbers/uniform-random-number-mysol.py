1a6cbd3884d7c823c7e5aa0281974be55eedb4d1
jwl-7 <lucidrainbows@gmail.com>
2019-05-22 03:09:49 -0400

    added my solution

--- uniform_random_numbers/uniform-random-number-mysol.py
+++ uniform_random_numbers/uniform-random-number-mysol.py
@@ -1,6 +1,7 @@
-"""[FILL IN] Algorithm
+"""Rejection Sampling Algorithm
 
-Time Complexity: [FILL IN]
+ Time Complexity: O(log(b - a + 1))
+Space Complexity: O(1)
 """
 
 import random
@@ -10,25 +11,25 @@ class Solution:
         return random.randrange(2)
 
     def uniform_random(self, lower_bound, upper_bound):
-        # TODO - you fill in here.
-        return 0
+        p = upper_bound - lower_bound + 1
+        while True:
+            x = random.randrange(p)
+            y = self.zero_one_random()
+            if y < p:
+                break
+        return x
     
 def main():
     s = Solution()
     test_cases = [
         (1, 6),
-        (1, 6),
-        (1, 6),
-        (0, 3),
-        (0, 3),
-        (0, 3),
-        (1, 10),
-        (1, 10),
-        (1, 10)
+        (1, 2),
+        (0, 10)
         ]
     for num in test_cases:
-        random_num = s.uniform_random(num[0], num[1])
-        print(f'[{num[0]},{num[1]}] -> {random_num}')
+        for i in range(3):
+            random_num = s.uniform_random(num[0], num[1])
+            print(f'[{num[0]},{num[1]}] -> {random_num}')
 
 if __name__ == '__main__':
     main()
