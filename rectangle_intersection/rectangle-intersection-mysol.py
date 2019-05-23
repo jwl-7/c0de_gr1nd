d4f83aac6f054a58f107ee7c326a6bb606c6d4c7
jwl-7 <lucidrainbows@gmail.com>
2019-05-22 23:05:40 -0400

    added working intersect detection

--- rectangle_intersection/rectangle-intersection-mysol.py
+++ rectangle_intersection/rectangle-intersection-mysol.py
@@ -1,4 +1,4 @@
-"""Rectangle Nonempty Intersection Checker
+"""Rectangle Intersect Check
 
 Time Complexity: O(1)
 """
@@ -8,9 +8,14 @@ import collections
 Rectangle = collections.namedtuple('Rectangle', ('x', 'y', 'width', 'height'))
 
 class Solution:
+    def is_intersect(self, R1, R2):
+        return not (R1.x + R1.width <= R2.x or
+                    R1.x >= R2.x + R2.width or
+                    R1.y >= R2.y + R2.height or
+                    R1.y + R1.height <= R2.y)
+
     def intersect_rectangle(self, R1, R2):
-        # TODO - you fill in here.
-        return Rectangle(0, 0, 0, 0)
+        return self.is_intersect(R1, R2)
     
 def main():
     s = Solution()
@@ -21,9 +26,11 @@ def main():
         ([17, 77, 58, 67], [5, 19, 32, 60])
         ]
     for num in test_cases:
-        intersect = s.intersect_rectangle(Rectangle(num[0][0],num[0][1],num[0][2],num[0][3]), 
-                                          Rectangle(num[1][0],num[1][1],num[1][2],num[1][3]))
-        print(intersect)
+        intersect = s.intersect_rectangle(Rectangle(num[0][0], num[0][1], num[0][2], num[0][3]), 
+                                          Rectangle(num[1][0], num[1][1], num[1][2], num[1][3]))
+        print(f'R1 = [{num[0][0]}, {num[0][1]}, {num[0][2]}, {num[0][3]}]')
+        print(f'R2 = [{num[1][0]}, {num[1][1]}, {num[1][2]}, {num[1][3]}]')
+        print(f'Intersect? {intersect}\n')
 
 if __name__ == '__main__':
     main()
