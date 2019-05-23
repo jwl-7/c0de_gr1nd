02f6c908f8ff548df415a965ea69e7ff2fffb54d
jwl-7 <lucidrainbows@gmail.com>
2019-05-23 05:18:58 -0400

    added skeleton for my solution

---
+++ dutch_national_flag/dutch-national-flag-mysol.py
@@ -0,0 +1,28 @@
+"""[FILL IN] Algorithm
+
+Time Complexity: O(n)
+"""
+
+class Solution:
+    def dutch_flag_partition(self, pivot_index, A):
+        pivot = A[pivot_index]
+        return A
+
+    
+def main():
+    s = Solution()
+    test_cases = [
+        (1, [1, 1, 0, 2]),
+        (3, [0, 1, 2, 0, 0, 1, 2]),
+        (0, [2, 0, 1, 0, 1, 0, 2, 0]),
+        (6, [2, 0, 1, 2, 2, 1, 1, 2, 1]),
+        (8, [2, 0, 1, 2, 2, 0, 2, 2, 1, 2, 1, 1, 2, 1])
+        ]
+    for num in test_cases:
+        print(f'PIVOT INDEX: {num[0]}')
+        print(f'BEFORE: {num[1]}')
+        flag = s.dutch_flag_partition(num[0], num[1])
+        print(f'AFTER: {flag}\n')
+
+if __name__ == '__main__':
+    main()
