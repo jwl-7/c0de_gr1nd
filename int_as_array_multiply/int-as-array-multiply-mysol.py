4b5030bc1adafdbcc395a51cd1b0a8b84108d511
jwl-7 <lucidrainbows@gmail.com>
2019-05-23 19:54:10 -0400

    added negative check

--- int_as_array_multiply/int-as-array-multiply-mysol.py
+++ int_as_array_multiply/int-as-array-multiply-mysol.py
@@ -14,13 +14,24 @@ class Solution:
         return num
 
     def multiply(self, num1, num2):
+        negative = False
+        if num1[0] < 0 or num2[0] < 0:
+            negative = True
+            if num1[0] < 0 and num2[0] < 0:
+                negative = False
+            num1[0] = abs(num1[0])
+            num2[0] = abs(num2[0])
+
         product = [0] * len(num1 + num2)
         for i in reversed(range(len(num1))):
             for j in reversed(range(len(num2))):
                 product[i + j + 1] += num1[i] * num2[j]
                 product[i + j] += product[i + j + 1] // 10
                 product[i + j + 1] %= 10
+
         product = self.remove_leading_zeroes(product)
+        if negative:
+            product[0] = -product[0]
         return product
     
 def main():
@@ -28,7 +39,8 @@ def main():
     test_cases = [
         ([1, 2, 3], [3, 2, 1]),
         ([1, 2, 9], [1, 3, 0]),
-        ([7, 0, 4, 9], [1, 9, 8, 4]),
+        ([-7, 0, 4, 9], [-1, 9, 8, 4]),
+        ([-6, 1, 3, 7, 8], [4, 5, 2, 4, 9]),
         ([1, 2, 3, 4, 5, 6, 7], [7, 6, 5, 4, 3, 2, 1]),
         ([3, 3, 2, 8, 3, 9, 1, 1, 6], [3, 3, 2, 4, 8, 9, 1, 1, 7])
         ]
