f51fc9839a599ab226f7521dd603c5144272971c
jwl-7 <lucidrainbows@gmail.com>
2019-05-28 02:50:58 -0400

    added my solution

--- remove_duplicates_sorted_array/sorted-array-remove-dups-mysol.py
+++ remove_duplicates_sorted_array/sorted-array-remove-dups-mysol.py
@@ -1,25 +1,31 @@
-"""[FILL IN] Algorithm
+"""Two Pointers Algorithm
 
-Time Complexity: [FILL IN]
+ Time Complexity: O(n)
+Space Complexity: O(1)
 """
 
 class Solution:
     def delete_duplicates(self, A):
-        # TODO - you fill in here.
-        return 0
+        valid = 1
+        for i in range(1, len(A)):
+            if A[i] != A[valid - 1]:
+                valid += 1
+                A[valid - 1] = A[i]
+        return valid
     
 def main():
     s = Solution()
     test_cases = [
         [-1, -1, 0, 1, 1, 2, 2, 3],
         [-4, -2, -2, 1, 1, 1, 2, 3],
+        [2, 3, 5, 5, 7, 11, 11, 11, 13],
         [-4, -3, -3, -2, -1, 0, 1, 2, 2, 3, 3, 4, 5, 5],
         [-8, -7, -6, -5, -5, -4, -3, -1, -1, 0, 0, 2, 2, 2, 4],
-        [-10, -8, -4, -2, -2, -1, -1, -1, 0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4]
         ]
     for num in test_cases:
+        print(f'      A = {num}')
         num_valid_elements = s.delete_duplicates(num)
-        print(f'{num}')
+        print(f'Updated = {num}')
         print(f'Valid Elements = {num_valid_elements}\n')
 
 if __name__ == '__main__':
