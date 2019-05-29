d057c13055cb9279f63af50743947302de917b52
jwl-7 <lucidrainbows@gmail.com>
2019-05-29 16:19:23 -0400

    added working epi test

--- temp/power_x_y/power-x-y-mysol.py
+++ temp/power_x_y/power-x-y-mysol.py
@@ -3,6 +3,8 @@
 Time Complexity: O(n)
 """
 
+from . test_framework import generic_test
+
 class Solution:
     def power(self, x, y):
         result = 1
@@ -16,17 +18,6 @@ class Solution:
             y >>= 1
         return result
     
-def main():
-    s = Solution()
-    test_cases = [
-        (9.871778445549765, 3),
-        (1.0005205926241314, 2),
-        (3.398860582608009, 4),
-        (11.274393136707644, 5)
-        ]
-    for num in test_cases:
-        result = s.power(num[0], num[1])
-        print(f'{num[0]} ^ {num[1]} = {result}')
-
 if __name__ == '__main__':
-    main()
+    exit(
+        generic_test.generic_test_main("power_x_y.py", 'power_x_y.tsv', power))
