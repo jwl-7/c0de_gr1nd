bac0f6070dc437daa900b7355c365f6e049f12ee
jwl-7 <lucidrainbows@gmail.com>
2019-05-29 05:08:36 -0400

    added my solution

--- buy_and_sell_stock_twice/buy-and-sell-stock-twice-mysol.py
+++ buy_and_sell_stock_twice/buy-and-sell-stock-twice-mysol.py
@@ -1,4 +1,4 @@
-"""[FILL IN] Algorithm
+"""Two Phases Algorithm
 
  Time Complexity: O(n)
 Space Complexity: O(n)
@@ -6,8 +6,20 @@ Space Complexity: O(n)
 
 class Solution:
     def buy_and_sell_stock_twice(self, prices):
-        # TODO - you fill in here.
-        return 0.0
+        profits = []
+        min_price = float('inf')
+        max_price = float('-inf')
+        max_profit = 0.0
+        for price in prices:
+            min_price = min(min_price, price)
+            profit = price - min_price
+            max_profit = max(max_profit, profit)
+            profits.append(max_profit)
+        for i, price in reversed(list(enumerate(prices[1:]))):
+            max_price = max(max_price, price)
+            profit = max_price - price
+            max_profit = max(max_profit, profit + profits[i])
+        return max_profit
     
 def main():
     s = Solution()
