5c617d99ec29307f83ea52661da3b7c43466979e
jwl-7 <lucidrainbows@gmail.com>
2019-05-29 23:36:05 -0400

    changed solution to use bit manipulation

--- python_solutions/reverse_bits.py
+++ python_solutions/reverse_bits.py
@@ -2,7 +2,15 @@ from test_framework import generic_test
 
 
 def reverse_bits(x):
-    return int(bin(x)[2:].zfill(64)[::-1], 2)
+    j = 63
+    for i in range(32):
+        lsb = x >> i & 1
+        msb = x >> j & 1
+        if lsb != msb:
+            x ^= 1 << i
+            x ^= 1 << j
+        j -= 1
+    return x
 
 
 if __name__ == '__main__':
