5893a8b460e803fd6866b1ced6a012c0978699e7
jwl-7 <lucidrainbows@gmail.com>
2019-05-30 14:17:47 -0400

    formatting

--- python_solutions/uniform_random_number.py
+++ python_solutions/uniform_random_number.py
@@ -2,8 +2,7 @@ import functools
 import random
 
 from test_framework import generic_test
-from test_framework.random_sequence_checker import (
-    check_sequence_is_uniformly_random, run_func_with_retries)
+from test_framework.random_sequence_checker import (check_sequence_is_uniformly_random, run_func_with_retries)
 from test_framework.test_utils import enable_executor_hook
 
 
@@ -27,18 +26,8 @@ def uniform_random(lower_bound, upper_bound):
 def uniform_random_wrapper(executor, lower_bound, upper_bound):
     def uniform_random_runner(executor, lower_bound, upper_bound):
         result = executor.run(lambda : [uniform_random(lower_bound, upper_bound) for _ in range(100000)])
-
-        return check_sequence_is_uniformly_random(
-            [a - lower_bound
-             for a in result], upper_bound - lower_bound + 1, 0.01)
-
-    run_func_with_retries(
-        functools.partial(uniform_random_runner, executor, lower_bound,
-                          upper_bound))
-
+        return check_sequence_is_uniformly_random([a - lower_bound for a in result], upper_bound - lower_bound + 1, 0.01)
+    run_func_with_retries(functools.partial(uniform_random_runner, executor, lower_bound, upper_bound))
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("uniform_random_number.py",
-                                       'uniform_random_number.tsv',
-                                       uniform_random_wrapper))
+    exit(generic_test.generic_test_main("uniform_random_number.py", 'uniform_random_number.tsv', uniform_random_wrapper))
