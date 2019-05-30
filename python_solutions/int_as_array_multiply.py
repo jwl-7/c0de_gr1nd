240f3da5300a839923e14c5671df46f0d8e953b4
jwl-7 <lucidrainbows@gmail.com>
2019-05-30 15:24:13 -0400

    formatting

--- python_solutions/int_as_array_multiply.py
+++ python_solutions/int_as_array_multiply.py
@@ -32,6 +32,4 @@ def multiply(num1, num2):
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("int_as_array_multiply.py",
-                                       'int_as_array_multiply.tsv', multiply))
+    exit(generic_test.generic_test_main("int_as_array_multiply.py", 'int_as_array_multiply.tsv', multiply))
