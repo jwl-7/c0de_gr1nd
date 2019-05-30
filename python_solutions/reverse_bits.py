d4b0c040ed2447fcea3243dc2605524e05491440
jwl-7 <lucidrainbows@gmail.com>
2019-05-29 21:38:20 -0400

    formatting

--- python_solutions/reverse_bits.py
+++ python_solutions/reverse_bits.py
@@ -6,6 +6,4 @@ def reverse_bits(x):
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("reverse_bits.py", "reverse_bits.tsv",
-                                       reverse_bits))
+    exit(generic_test.generic_test_main("reverse_bits.py", "reverse_bits.tsv", reverse_bits))
