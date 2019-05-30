01610a3c4355def2e9ea028acab88e0e2bfe022a
jwl-7 <lucidrainbows@gmail.com>
2019-05-30 16:00:58 -0400

    formatting

--- python_solutions/int_as_array_increment.py
+++ python_solutions/int_as_array_increment.py
@@ -15,6 +15,4 @@ def plus_one(A):
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("int_as_array_increment.py",
-                                       "int_as_array_increment.tsv", plus_one))
+    exit(generic_test.generic_test_main("int_as_array_increment.py", "int_as_array_increment.tsv", plus_one))
