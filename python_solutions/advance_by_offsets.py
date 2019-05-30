dcf91974b9ffe58d4d0e227a60d38cfce0d6c426
jwl-7 <lucidrainbows@gmail.com>
2019-05-30 15:48:17 -0400

    formatting

--- python_solutions/advance_by_offsets.py
+++ python_solutions/advance_by_offsets.py
@@ -12,6 +12,4 @@ def can_reach_end(A):
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main(
-            "advance_by_offsets.py", "advance_by_offsets.tsv", can_reach_end))
+    exit(generic_test.generic_test_main("advance_by_offsets.py", "advance_by_offsets.tsv", can_reach_end))
