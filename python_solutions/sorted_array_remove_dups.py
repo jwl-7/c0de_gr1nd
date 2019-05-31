bbdae4f4c268b501d331c920e5b33d5883ce8ac3
jwl-7 <lucidrainbows@gmail.com>
2019-05-30 20:29:14 -0400

    formatting

--- python_solutions/sorted_array_remove_dups.py
+++ python_solutions/sorted_array_remove_dups.py
@@ -4,7 +4,7 @@ from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
 
 
-# Returns the number of valid entries after deletion.
+
 def delete_duplicates(A):
     valid = 0
     for i in range(1, len(A)):
@@ -19,9 +19,7 @@ def delete_duplicates_wrapper(executor, A):
     idx = executor.run(functools.partial(delete_duplicates, A))
     return A[:idx]
 
-
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("sorted_array_remove_dups.py",
-                                       'sorted_array_remove_dups.tsv',
-                                       delete_duplicates_wrapper))
+    exit(generic_test.generic_test_main("sorted_array_remove_dups.py",
+                                        'sorted_array_remove_dups.tsv',
+                                        delete_duplicates_wrapper))
