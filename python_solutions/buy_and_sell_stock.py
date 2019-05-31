84ba2af30999f66e3de7eec9b4d28eb872df8bce
jwl-7 <lucidrainbows@gmail.com>
2019-05-30 20:53:53 -0400

    formatting

--- python_solutions/buy_and_sell_stock.py
+++ python_solutions/buy_and_sell_stock.py
@@ -9,10 +9,7 @@ def buy_and_sell_stock_once(prices):
         profit = price - min_price
         max_profit = max(max_profit, profit)
     return max_profit
-    
+
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("buy_and_sell_stock.py",
-                                       "buy_and_sell_stock.tsv",
-                                       buy_and_sell_stock_once))
+    exit(generic_test.generic_test_main("buy_and_sell_stock.py", "buy_and_sell_stock.tsv", buy_and_sell_stock_once))
