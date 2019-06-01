26e92a2a0ca4c703b23e8169e079d6ee29b092c8
jwl-7 <lucidrainbows@gmail.com>
2019-06-01 15:21:36 -0400

    added solution

--- python_solutions/next_permutation.py
+++ python_solutions/next_permutation.py
@@ -2,8 +2,17 @@ from test_framework import generic_test
 
 
 def next_permutation(perm):
-    # TODO - you fill in here.
-    return []
+    i = len(perm) - 2
+    while i >= 0 and perm[i] >= perm[i + 1]:
+        i -= 1
+    if i == -1:
+        return []
+    for j in reversed(range(i + 1, len(perm))):
+        if perm[j] > perm[i]:
+            perm[i], perm[j] = perm[j], perm[i]
+            break
+    perm[i + 1:len(perm) + 1] = perm[i + 1:len(perm) + 1][::-1]
+    return perm
 
 
 if __name__ == '__main__':
