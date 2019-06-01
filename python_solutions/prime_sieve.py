c6e92c9121d7ac62bebf7f8e57c971bfa94037fc
jwl-7 <lucidrainbows@gmail.com>
2019-05-31 21:42:03 -0400

    added trial by division solution

--- python_solutions/prime_sieve.py
+++ python_solutions/prime_sieve.py
@@ -1,9 +1,20 @@
 from test_framework import generic_test
 
 
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
 def generate_primes(n):
-    # TODO - you fill in here.
-    return []
+    primes = []
+    for i in range(1, n + 1):
+        if is_prime(i):
+            primes.append(i)
+    return primes
 
 
 if __name__ == '__main__':
