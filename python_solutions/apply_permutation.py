43c3dd92651c5b8401b6da115f2859f56bcc3531
jwl-7 <lucidrainbows@gmail.com>
2019-06-01 02:40:08 -0400

    formatting

--- python_solutions/apply_permutation.py
+++ python_solutions/apply_permutation.py
@@ -10,9 +10,5 @@ def apply_permutation_wrapper(perm, A):
     apply_permutation(perm, A)
     return A
 
-
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main("apply_permutation.py",
-                                       "apply_permutation.tsv",
-                                       apply_permutation_wrapper))
+    exit(generic_test.generic_test_main("apply_permutation.py", "apply_permutation.tsv", apply_permutation_wrapper))
