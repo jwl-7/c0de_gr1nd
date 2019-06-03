228ec7a803adb562865a68e1c47a2d82f68a8ac5
jwl-7 <lucidrainbows@gmail.com>
2019-06-02 20:14:21 -0400

    added successful check for columns and rows

--- python_solutions/is_valid_sudoku.py
+++ python_solutions/is_valid_sudoku.py
@@ -1,9 +1,20 @@
 from test_framework import generic_test
+from collections import Counter
 
 
-# Check if a partially filled matrix has any conflicts.
 def is_valid_sudoku(partial_assignment):
-    # TODO - you fill in here.
+    # CHECK COLUMNS
+    for i in range(len(partial_assignment)):
+        for num, count in Counter([k[i] for k in partial_assignment]).items():
+            if num != 0 and count > 1:
+                return False
+
+    # CHECK ROWS
+    for i in range(len(partial_assignment)):
+        for num, count in Counter(partial_assignment[i]).items():
+            if num != 0 and count > 1:
+                return False
+    
     return True
 
 
