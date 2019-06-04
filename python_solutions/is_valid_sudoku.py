544b99a4a3599e9a1b45d6a1c678a912b5ca1c11
jwl-7 <lucidrainbows@gmail.com>
2019-06-03 20:01:28 -0400

    formatting

--- python_solutions/is_valid_sudoku.py
+++ python_solutions/is_valid_sudoku.py
@@ -7,14 +7,15 @@ def is_valid_list(section):
 
 def is_valid_sudoku(grid):
     for i in range(9):
-        if (not is_valid_list(grid[i]) or 
-            not is_valid_list([col[i] for col in grid])):
-                return False
+        if (not is_valid_list(grid[i]) or not is_valid_list([col[i] for col in grid])):
+            return False
     for i in range(3):
         for j in range(3):
-            if not is_valid_list(grid[a][b]
-                                 for b in range(i * 3, 3 * (i + 1)) 
-                                 for a in range(j * 3, 3 * (j + 1))):
+            if not is_valid_list(
+                grid[a][b]
+                for a in range(j * 3, 3 * (j + 1))
+                for b in range(i * 3, 3 * (i + 1))
+            ):
                 return False
     return True
 
