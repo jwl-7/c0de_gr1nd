508430572ed797497f13205fdc1e2cc4c88e9da8
jwl-7 <lucidrainbows@gmail.com>
2019-06-03 21:40:46 -0400

    added working solution

--- python_solutions/spiral_ordering_segments.py
+++ python_solutions/spiral_ordering_segments.py
@@ -2,8 +2,26 @@ from test_framework import generic_test
 
 
 def matrix_in_spiral_order(square_matrix):
-    # TODO - you fill in here.
-    return []
+    spiral = []
+    if square_matrix == []:
+        return spiral
+    row_start = 0
+    col_start = 0
+    row_end = len(square_matrix) - 1
+    col_end = len(square_matrix[0]) - 1
+
+    while row_start <= row_end and col_start <= col_end:
+        spiral.extend([square_matrix[row_start][i] for i in range(col_start, col_end + 1)])
+        spiral.extend([square_matrix[i][col_end] for i in range(row_start + 1, row_end + 1)])
+        if row_start < row_end:
+            spiral.extend([square_matrix[row_end][i] for i in reversed(range(col_start, col_end))])
+        if col_start < col_end:
+            spiral.extend([square_matrix[i][col_start] for i in reversed(range(row_start + 1, row_end))])
+        row_start += 1
+        col_start += 1
+        row_end -= 1
+        col_end -= 1
+    return spiral
 
 
 if __name__ == '__main__':
