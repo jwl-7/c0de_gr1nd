d86efa889171a251d738ee3b06dbfba91e6467cf
jwl-7 <lucidrainbows@gmail.com>
2019-09-23 02:50:36 -0400

    Add pythonic solution

--- python_solutions/reverse_bits.py
+++ python_solutions/reverse_bits.py
@@ -13,5 +13,9 @@ def reverse_bits(x):
     return x
 
 
+def reverse_bits_pythonic(x):
+    return int(bin(x)[2:].zfill(64)[::-1], 2)
+
+
 if __name__ == '__main__':
     exit(generic_test.generic_test_main("reverse_bits.py", "reverse_bits.tsv", reverse_bits))
