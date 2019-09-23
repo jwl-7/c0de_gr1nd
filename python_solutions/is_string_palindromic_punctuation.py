d0d3aebf8a757a829455c6c96a6fc109d1ef8df8
jwl-7 <lucidrainbows@gmail.com>
2019-09-23 02:53:48 -0400

    Add solution and pythonic solution

--- python_solutions/is_string_palindromic_punctuation.py
+++ python_solutions/is_string_palindromic_punctuation.py
@@ -2,8 +2,23 @@ from test_framework import generic_test
 
 
 def is_palindrome(s):
-    # TODO - you fill in here.
+    start = 0
+    end = len(s) - 1
+    while start < end:
+        while start < end and not s[start].isalnum():
+            start += 1
+        while start < end and not s[end].isalnum():
+            end -= 1
+        if s[start].lower() != s[end].lower():
+            return False
+        start += 1
+        end -= 1
     return True
+
+
+def is_palindrome_pythonic(s):
+    s = [x.lower() for x in s if x.isalnum()]
+    return s == s[::-1]
 
 
 if __name__ == '__main__':
