86c32cb6a769763eeabeecc156c40978952916ce
jwl-7 <lucidrainbows@gmail.com>
2019-09-23 20:42:01 -0400

    Add math-based solution

--- python_solutions/reverse_digits.py
+++ python_solutions/reverse_digits.py
@@ -2,6 +2,15 @@ from test_framework import generic_test
 
 
 def reverse(x):
+    r = 0
+    temp = abs(x)
+    while temp:
+        r = r * 10 + temp % 10
+        temp //= 10
+    return -r if x < 0 else r
+
+
+def reverse_pythonic(x):
     return -int(str(abs(x))[::-1]) if x < 0 else int(str(x)[::-1])
 
 
