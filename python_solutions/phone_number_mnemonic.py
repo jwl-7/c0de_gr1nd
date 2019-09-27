ac16b4ed974e26f3a2c5cd884949fb0e375c8601
jwl-7 <lucidrainbows@gmail.com>
2019-09-26 23:20:09 -0400

    Add recursive solution

--- python_solutions/phone_number_mnemonic.py
+++ python_solutions/phone_number_mnemonic.py
@@ -19,10 +19,37 @@ def phone_mnemonic(phone_number):
     return mnemonics
 
 
+def phone_mnemonic_recursive(phone_number):
+    keypad = {
+        '2': 'ABC',
+        '3': 'DEF',
+        '4': 'GHI',
+        '5': 'JKL',
+        '6': 'MNO',
+        '7': 'PQRS',
+        '8': 'TUV',
+        '9': 'WXYZ',
+    }
+    
+    def build_mnemonic(num):
+        if num == len(phone_number):
+            mnemonics.append(''.join(curr))
+        else:
+            letters = keypad.get(phone_number[num], phone_number[num])
+            for letter in letters:
+                curr[num] = letter
+                build_mnemonic(num + 1)
+
+    mnemonics = []
+    curr = [0] * len(phone_number)
+    build_mnemonic(0)
+    return mnemonics
+
+
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main(
             "phone_number_mnemonic.py",
             'phone_number_mnemonic.tsv',
-            phone_mnemonic,
+            phone_mnemonic_recursive,
             comparator=test_utils.unordered_compare))
