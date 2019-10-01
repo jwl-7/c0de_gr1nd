d767fc30ebd7310275defef17dca34c77b66c52f
jwl-7 <lucidrainbows@gmail.com>
2019-10-01 19:31:53 -0400

    Minor syntax change

--- python_solutions/valid_ip_addresses.py
+++ python_solutions/valid_ip_addresses.py
@@ -6,7 +6,7 @@ def get_valid_ip_address(s):
     for a in range(1, 4):
         for b in range(a + 1, a + 4):
             for c in range(b + 1, b + 4):
-                if 0 < len(s) - c < 4:
+                if 1 <= len(s) - c <= 3:
                     tmp = (s[:a], s[a:b], s[b:c], s[c:])
                     if all(int(x) <= 255 and str(int(x)) == x for x in tmp):
                         ips.append('.'.join(tmp))
