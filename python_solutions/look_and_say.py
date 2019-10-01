22e70d7252a1ae25b38f0ac3b8ddee42dfe4b91f
jwl-7 <lucidrainbows@gmail.com>
2019-09-30 20:54:08 -0400

    Add solution

--- python_solutions/look_and_say.py
+++ python_solutions/look_and_say.py
@@ -2,8 +2,21 @@ from test_framework import generic_test
 
 
 def look_and_say(n):
-    # TODO - you fill in here.
-    return ''
+    s = '1'
+    for _ in range(1, n):
+        temp = ''
+        curr = s[0]
+        count = 0
+        for char in s:
+            if curr == char:
+                count += 1
+            else:
+                temp += str(count) + curr
+                curr = char
+                count = 1     
+        temp += str(count) + char
+        s = temp
+    return s
 
 
 if __name__ == '__main__':
