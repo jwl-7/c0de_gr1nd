8a443c1f140e1b82e78e6f8258876b5cf6402d5d
jwl-7 <lucidrainbows@gmail.com>
2019-10-01 18:01:07 -0400

    Add solution

--- python_solutions/valid_ip_addresses.py
+++ python_solutions/valid_ip_addresses.py
@@ -2,8 +2,15 @@ from test_framework import generic_test
 
 
 def get_valid_ip_address(s):
-    # TODO - you fill in here.
-    return []
+    ips = []
+    for a in range(1, 4):
+        for b in range(a + 1, a + 4):
+            for c in range(b + 1, b + 4):
+                if 0 < len(s) - c < 4:
+                    tmp = (s[:a], s[a:b], s[b:c], s[c:])
+                    if all(int(x) <= 255 and str(int(x)) == x for x in tmp):
+                        ips.append('.'.join(tmp))
+    return ips
 
 
 def comp(a, b):
