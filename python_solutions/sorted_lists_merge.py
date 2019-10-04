632a7a546b07a2d15343df84f46d9a1ecb296b5d
jwl-7 <lucidrainbows@gmail.com>
2019-10-03 21:01:00 -0400

    Add solution

--- python_solutions/sorted_lists_merge.py
+++ python_solutions/sorted_lists_merge.py
@@ -1,9 +1,19 @@
 from test_framework import generic_test
+from list_node import ListNode
 
 
 def merge_two_sorted_lists(L1, L2):
-    # TODO - you fill in here.
-    return None
+    tail = head = ListNode()
+    while L1 and L2:
+        if L1.data < L2.data:
+            tail.next = L1
+            L1 = L1.next
+        else:
+            tail.next = L2
+            L2 = L2.next
+        tail = tail.next
+    tail.next = L1 or L2
+    return head.next
 
 
 if __name__ == '__main__':
