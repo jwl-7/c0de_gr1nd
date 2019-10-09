1917931c54343429ed67a4f271764816eeb9bbc8
jwl-7 <lucidrainbows@gmail.com>
2019-10-09 17:11:02 -0400

    Add initial bad solution

--- python_solutions/even_odd_list_merge.py
+++ python_solutions/even_odd_list_merge.py
@@ -1,9 +1,24 @@
 from test_framework import generic_test
+from list_node import ListNode
 
 
 def even_odd_merge(L):
-    # TODO - you fill in here.
-    return None
+    even = teven = ListNode()
+    odd = todd = ListNode()
+    i = 0
+    while L:
+        if i & 1:
+            todd.next = L
+            L = L.next
+            todd = todd.next
+        else:
+            teven.next = L
+            L = L.next
+            teven = teven.next
+        i += 1
+    todd.next = None
+    teven.next = odd.next
+    return even.next
 
 
 if __name__ == '__main__':
