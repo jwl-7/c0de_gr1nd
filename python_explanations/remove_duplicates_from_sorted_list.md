814e93a1727ab98e4819082680d97f8fa4781a50
jwl-7 <lucidrainbows@gmail.com>
2019-10-08 22:22:05 -0400

    Add solution

--- python_explanations/remove_duplicates_from_sorted_list.md
+++ python_explanations/remove_duplicates_from_sorted_list.md
@@ -9,7 +9,14 @@ Output: L -> [1] -> [2] -> [3] -> [5] -> [7] -> [11] -> None
   
 ## Solution
 ```python
-BLANK
+def remove_duplicates(L):
+    curr = L
+    while curr and curr.next:
+        if curr.data == curr.next.data:
+            curr.next = curr.next.next
+        else:
+            curr = curr.next
+    return L
 ```
   
 ## Explanation
--- python_solutions/remove_duplicates_from_sorted_list.py
+++ python_solutions/remove_duplicates_from_sorted_list.py
@@ -2,8 +2,13 @@ from test_framework import generic_test
 
 
 def remove_duplicates(L):
-    # TODO - you fill in here.
-    return None
+    curr = L
+    while curr and curr.next:
+        if curr.data == curr.next.data:
+            curr.next = curr.next.next
+        else:
+            curr = curr.next
+    return L
 
 
 if __name__ == '__main__':
