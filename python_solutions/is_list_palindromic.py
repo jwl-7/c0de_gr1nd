cd6c98a3d5a33b41d8aba6a84a3cd58291043e9a
jwl-7 <lucidrainbows@gmail.com>
2019-10-09 21:04:36 -0400

    Add solution

--- python_solutions/is_list_palindromic.py
+++ python_solutions/is_list_palindromic.py
@@ -1,8 +1,31 @@
 from test_framework import generic_test
 
 
+def reverse_linked_list(head):
+    prev = None
+    curr = head
+    while curr:
+        nxt = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nxt
+    return prev
+
+
 def is_linked_list_a_palindrome(L):
-    # TODO - you fill in here.
+    slow = fast = L
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+
+    a = L
+    b = reverse_linked_list(slow)
+
+    while b:
+        if a.data != b.data:
+            return False
+        a = a.next
+        b = b.next
     return True
 
 
