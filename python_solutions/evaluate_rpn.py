0e9a0e49839a3eb7368f365f93fe62b6100e774a
jwl-7 <lucidrainbows@gmail.com>
2019-10-15 20:08:27 -0400

    Add solution

--- python_solutions/evaluate_rpn.py
+++ python_solutions/evaluate_rpn.py
@@ -2,8 +2,24 @@ from test_framework import generic_test
 
 
 def evaluate(expression):
-    # TODO - you fill in here.
-    return 0
+    stack = []
+    operators = '+-*/'
+    for token in expression.split(','):
+        if token not in operators:
+            stack.append(int(token))
+        else:
+            y = stack.pop()
+            x = stack.pop()
+            if token == '+':
+                result = x + y
+            elif token == '-':
+                result = x - y
+            elif token == '*':
+                result = x * y
+            elif token == '/':
+                result = int(x / y)
+            stack.append(result)
+    return stack[0]
 
 
 if __name__ == '__main__':
