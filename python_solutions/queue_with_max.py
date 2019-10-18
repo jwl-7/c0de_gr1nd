b6d8c686737e77a9e201e389eb66bda1ef39df12
jwl-7 <lucidrainbows@gmail.com>
2019-10-18 02:57:04 -0400

    Add solution

--- python_solutions/queue_with_max.py
+++ python_solutions/queue_with_max.py
@@ -2,18 +2,51 @@ from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
 
-class QueueWithMax:
-    def enqueue(self, x):
-        # TODO - you fill in here.
-        return
+class Stack:
 
-    def dequeue(self):
-        # TODO - you fill in here.
-        return 0
+    def __init__(self):
+        self.stack = []
+
+    def empty(self):
+        return self.stack == []
 
     def max(self):
-        # TODO - you fill in here.
-        return 0
+        if self.stack:
+            return self.stack[-1][1]
+
+    def pop(self):
+        if self.stack:
+            return self.stack.pop()[0]
+
+    def push(self, x):
+        curr_max = max(x, self.stack[-1][1] if self.stack else x)
+        self.stack.append((x, curr_max))
+
+
+class QueueWithMax:
+
+    def __init__(self):
+        self.input = Stack()
+        self.output = Stack()
+
+    def enqueue(self, x):
+        self.input.push(x)
+
+    def dequeue(self):
+        if self.output.empty():
+            while not self.input.empty():
+                self.output.push(self.input.pop())
+        if not self.output.empty():
+            return self.output.pop()
+
+    def max(self):
+        if not self.input.empty():
+            if self.output.empty():
+                return self.input.max()
+            else:
+                return max(self.input.max(), self.output.max())
+        elif not self.output.empty():
+            return self.output.max()
 
 
 def queue_tester(ops):
