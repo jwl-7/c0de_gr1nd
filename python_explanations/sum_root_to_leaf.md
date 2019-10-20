eca1b0473bceef81ddbfa133cd11215e692547db
jwl-7 <lucidrainbows@gmail.com>
2019-10-20 19:09:47 -0400

    Add solution

--- python_explanations/sum_root_to_leaf.md
+++ python_explanations/sum_root_to_leaf.md
@@ -21,7 +21,18 @@ The binary numbers represented by this tree are:
 
 ## Solution
 ```python
-BLANK
+def sum_root_to_leaf(tree, partial_path_sum=0):
+    if not tree:
+        return 0
+
+    partial_path_sum = partial_path_sum * 2 + tree.data
+    if not tree.left and not tree.right:
+        return partial_path_sum
+
+    return (
+        sum_root_to_leaf(tree.left, partial_path_sum) +
+        sum_root_to_leaf(tree.right, partial_path_sum)
+    )
 ```
 
 ## Explanation
--- python_solutions/sum_root_to_leaf.py
+++ python_solutions/sum_root_to_leaf.py
@@ -2,8 +2,17 @@ from test_framework import generic_test
 
 
 def sum_root_to_leaf(tree, partial_path_sum=0):
-    # TODO - you fill in here.
-    return 0
+    if not tree:
+        return 0
+
+    partial_path_sum = partial_path_sum * 2 + tree.data
+    if not tree.left and not tree.right:
+        return partial_path_sum
+
+    return (
+        sum_root_to_leaf(tree.left, partial_path_sum) +
+        sum_root_to_leaf(tree.right, partial_path_sum)
+    )
 
 
 if __name__ == '__main__':
