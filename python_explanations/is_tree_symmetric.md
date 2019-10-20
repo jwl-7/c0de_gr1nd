665d0ff2ee62a7c87f2192242fbadf29bced1b9e
jwl-7 <lucidrainbows@gmail.com>
2019-10-20 04:09:41 -0400

    Add recursive solution

--- python_explanations/is_tree_symmetric.md
+++ python_explanations/is_tree_symmetric.md
@@ -27,7 +27,16 @@ Output: False
 
 ## Solution
 ```python
-BLANK
+def is_mirror(n1, n2):
+    if not n1 and not n2:
+        return True
+    elif n1 and n2 and n1.data == n2.data:
+        return is_mirror(n1.right, n2.left) and is_mirror(n1.left, n2.right)
+    return False
+
+
+def is_symmetric(tree):
+    return is_mirror(tree, tree)
 ```
 
 ## Explanation
--- python_solutions/is_tree_symmetric.py
+++ python_solutions/is_tree_symmetric.py
@@ -1,9 +1,16 @@
 from test_framework import generic_test
 
 
+def is_mirror(n1, n2):
+    if not n1 and not n2:
+        return True
+    elif n1 and n2 and n1.data == n2.data:
+        return is_mirror(n1.right, n2.left) and is_mirror(n1.left, n2.right)
+    return False
+
+
 def is_symmetric(tree):
-    # TODO - you fill in here.
-    return True
+    return is_mirror(tree, tree)
 
 
 if __name__ == '__main__':
