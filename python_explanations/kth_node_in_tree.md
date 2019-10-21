06cc3fb831848712cc6b3b11746994fdf9ae0f33
jwl-7 <lucidrainbows@gmail.com>
2019-10-21 19:29:38 -0400

    Refactor solution

--- python_explanations/kth_node_in_tree.md
+++ python_explanations/kth_node_in_tree.md
@@ -21,11 +21,11 @@ Output: 5
 def find_kth_node_binary_tree(tree, k):
     while tree:
         left_size = tree.left.size if tree.left else 0
-        if left_size + 1 < k:
-            k -= left_size + 1
-            tree = tree.right
-        elif left_size == k - 1:
+        if k == left_size + 1:
             return tree
+        elif k > left_size + 1:
+            tree = tree.right
+            k -= left_size + 1
         else:
             tree = tree.left
     return None
--- python_solutions/kth_node_in_tree.py
+++ python_solutions/kth_node_in_tree.py
@@ -16,11 +16,11 @@ class BinaryTreeNode:
 def find_kth_node_binary_tree(tree, k):
     while tree:
         left_size = tree.left.size if tree.left else 0
-        if left_size + 1 < k:
-            k -= left_size + 1
-            tree = tree.right
-        elif left_size == k - 1:
+        if k == left_size + 1:
             return tree
+        elif k > left_size + 1:
+            tree = tree.right
+            k -= left_size + 1
         else:
             tree = tree.left
     return None
