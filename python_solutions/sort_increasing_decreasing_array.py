d0544d39c9663047f0610c1b4cd8596bea383d63
jwl-7 <lucidrainbows@gmail.com>
2019-10-26 21:56:53 -0400

    Change var name

--- python_solutions/sort_increasing_decreasing_array.py
+++ python_solutions/sort_increasing_decreasing_array.py
@@ -4,24 +4,24 @@ from test_framework import generic_test
 
 
 def sort_k_increasing_decreasing_array(A):
-    sub_arrays = []
+    sorted_arrays = []
     increasing = True
     start = 0
     for i in range(1, len(A)):
         if A[i] >= A[i-1] and not increasing:
-            sub_arrays.append(A[i-1:start-1:-1])
+            sorted_arrays.append(A[i-1:start-1:-1])
             increasing = True
             start = i
         elif A[i] < A[i-1] and increasing:
-            sub_arrays.append(A[start:i])
+            sorted_arrays.append(A[start:i])
             increasing = False
             start = i
         if i == len(A) - 1:
             if not increasing:
-                sub_arrays.append(A[:start-1:-1])
+                sorted_arrays.append(A[:start-1:-1])
             else:
-                sub_arrays.append(A[start:])
-    return list(heapq.merge(*sub_arrays))
+                sorted_arrays.append(A[start:])
+    return list(heapq.merge(*sorted_arrays))
 
 
 if __name__ == '__main__':
