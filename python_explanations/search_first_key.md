cc5d154f88ec1ee794d7021abb9246722e74aef7
jwl-7 <lucidrainbows@gmail.com>
2019-10-29 15:12:57 -0400

    Add solution

--- python_explanations/search_first_key.md
+++ python_explanations/search_first_key.md
@@ -14,7 +14,20 @@ Output: 8
 
 ## Solution
 ```python
-BLANK
+def search_first_of_k(A, k):
+    left = 0
+    right = len(A) - 1
+    result = -1
+    while left <= right:
+        mid = (left + right) // 2
+        if A[mid] == k:
+            result = mid
+            right = mid - 1
+        elif A[mid] < k:
+            left = mid + 1
+        elif A[mid] > k:
+            right = mid - 1
+    return result
 ```
 
 ## Explanation
--- python_solutions/search_first_key.py
+++ python_solutions/search_first_key.py
@@ -2,8 +2,19 @@ from test_framework import generic_test
 
 
 def search_first_of_k(A, k):
-    # TODO - you fill in here.
-    return 0
+    left = 0
+    right = len(A) - 1
+    result = -1
+    while left <= right:
+        mid = (left + right) // 2
+        if A[mid] == k:
+            result = mid
+            right = mid - 1
+        elif A[mid] < k:
+            left = mid + 1
+        elif A[mid] > k:
+            right = mid - 1
+    return result
 
 
 if __name__ == '__main__':
