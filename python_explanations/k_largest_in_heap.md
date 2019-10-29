7bd3846836bc434a203fff3dbe88decefa3cc8d5
jwl-7 <lucidrainbows@gmail.com>
2019-10-28 22:20:30 -0400

    Add solution

--- python_explanations/k_largest_in_heap.md
+++ python_explanations/k_largest_in_heap.md
@@ -14,7 +14,26 @@ Output: [8, 7]
 
 ## Solution
 ```python
-BLANK
+def k_largest_in_binary_heap(A, k):
+    if k <= 0:
+        return []
+
+    max_heap = []
+    result = []
+    max_heap.append((-A[0], 0))
+
+    for _ in range(k):
+        largest_idx = max_heap[0][1]
+        result.append(-heapq.heappop(max_heap)[0])
+
+        left = 2 * largest_idx + 1
+        if left < len(A):
+            heapq.heappush(max_heap, (-A[left], left))
+
+        right = 2 * largest_idx + 2
+        if right < len(A):
+            heapq.heappush(max_heap, (-A[right], right))
+    return result
 ```
 
 ## Explanation
--- python_solutions/k_largest_in_heap.py
+++ python_solutions/k_largest_in_heap.py
@@ -1,9 +1,28 @@
+import heapq
+
 from test_framework import generic_test, test_utils
 
 
 def k_largest_in_binary_heap(A, k):
-    # TODO - you fill in here.
-    return []
+    if k <= 0:
+        return []
+
+    max_heap = []
+    result = []
+    max_heap.append((-A[0], 0))
+
+    for _ in range(k):
+        largest_idx = max_heap[0][1]
+        result.append(-heapq.heappop(max_heap)[0])
+
+        left = 2 * largest_idx + 1
+        if left < len(A):
+            heapq.heappush(max_heap, (-A[left], left))
+
+        right = 2 * largest_idx + 2
+        if right < len(A):
+            heapq.heappush(max_heap, (-A[right], right))
+    return result
 
 
 if __name__ == '__main__':
