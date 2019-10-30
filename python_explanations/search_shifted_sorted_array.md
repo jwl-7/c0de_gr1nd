37da138f3822d7f903397c3b5dc07ec93394dccb
jwl-7 <lucidrainbows@gmail.com>
2019-10-30 16:03:56 -0400

    Add solution

--- python_explanations/search_shifted_sorted_array.md
+++ python_explanations/search_shifted_sorted_array.md
@@ -14,7 +14,16 @@ Output: 2
 
 ## Solution
 ```python
-BLANK
+def search_smallest(A):
+    left = 0
+    right = len(A) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if A[mid] > A[right]:
+            left = mid + 1
+        else:
+            right = mid
+    return left
 ```
 
 ## Explanation
--- python_solutions/search_shifted_sorted_array.py
+++ python_solutions/search_shifted_sorted_array.py
@@ -2,8 +2,15 @@ from test_framework import generic_test
 
 
 def search_smallest(A):
-    # TODO - you fill in here.
-    return 0
+    left = 0
+    right = len(A) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if A[mid] > A[right]:
+            left = mid + 1
+        else:
+            right = mid
+    return left
 
 
 if __name__ == '__main__':
