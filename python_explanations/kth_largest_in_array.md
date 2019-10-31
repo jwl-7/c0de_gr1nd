4f840385ebb84c651eddc618420fc5e94d02a469
jwl-7 <lucidrainbows@gmail.com>
2019-10-31 19:07:33 -0400

    Add solution

--- python_explanations/kth_largest_in_array.md
+++ python_explanations/kth_largest_in_array.md
@@ -14,7 +14,30 @@ Output: -5
 
 ## Solution
 ```python
-BLANK
+def find_kth_largest(k, A):
+    left = 0
+    right = len(A) - 1
+    while left <= right:
+        pivot = partition(A, left, right)
+        if pivot == k - 1:
+            return A[pivot]
+        elif pivot < k - 1:
+            left = pivot + 1
+        else:
+            right = pivot - 1
+
+
+def partition(A, left, right):
+    pivot_idx = random.randint(left, right)
+    pivot_val = A[pivot_idx]
+    new_pivot = left
+    A[pivot_idx], A[right] = A[right], A[pivot_idx]
+    for i in range(left, right):
+        if A[i] > pivot_val:
+            A[i], A[new_pivot] = A[new_pivot], A[i]
+            new_pivot += 1
+    A[right], A[new_pivot] = A[new_pivot], A[right]
+    return new_pivot
 ```
 
 ## Explanation
--- python_solutions/kth_largest_in_array.py
+++ python_solutions/kth_largest_in_array.py
@@ -1,3 +1,5 @@
+import random
+
 from test_framework import generic_test
 
 
@@ -5,8 +7,29 @@ from test_framework import generic_test
 # find_kth_largest(1, A) returns 3, find_kth_largest(2, A) returns 2,
 # find_kth_largest(3, A) returns 1, and find_kth_largest(4, A) returns -1.
 def find_kth_largest(k, A):
-    # TODO - you fill in here.
-    return 0
+    left = 0
+    right = len(A) - 1
+    while left <= right:
+        pivot = partition(A, left, right)
+        if pivot == k - 1:
+            return A[pivot]
+        elif pivot < k - 1:
+            left = pivot + 1
+        else:
+            right = pivot - 1
+
+
+def partition(A, left, right):
+    pivot_idx = random.randint(left, right)
+    pivot_val = A[pivot_idx]
+    new_pivot = left
+    A[pivot_idx], A[right] = A[right], A[pivot_idx]
+    for i in range(left, right):
+        if A[i] > pivot_val:
+            A[i], A[new_pivot] = A[new_pivot], A[i]
+            new_pivot += 1
+    A[right], A[new_pivot] = A[new_pivot], A[right]
+    return new_pivot
 
 
 if __name__ == '__main__':
