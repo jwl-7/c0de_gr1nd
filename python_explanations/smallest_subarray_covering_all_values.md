6bbc646b39c7c558e4672fcb5247cfe78aeeef8d
jwl-7 <lucidrainbows@gmail.com>
2019-11-05 18:27:42 -0500

    Add solution

--- python_explanations/smallest_subarray_covering_all_values.md
+++ python_explanations/smallest_subarray_covering_all_values.md
@@ -14,7 +14,28 @@ Output: Subarray(start=3, end=5)
 
 ## Solution
 ```python
-BLANK
+def find_smallest_sequentially_covering_subset(paragraph, keywords):
+    key_lookup = {}
+    for i, s in enumerate(keywords):
+        key_lookup[s] = i
+    recent = [-1] * len(keywords)
+    smallest = [float('inf')] * len(keywords)
+    shortest = float('inf')
+    start, end = 0, 0
+
+    for i, s in enumerate(paragraph):
+        if s in key_lookup:
+            key_idx = key_lookup[s]
+            if key_idx == 0:
+                smallest[key_idx] = 1
+            elif smallest[key_idx - 1] != float('inf'):
+                prev_dist = i - recent[key_idx - 1]
+                smallest[key_idx] = prev_dist + smallest[key_idx - 1]
+            recent[key_idx] = i
+            if key_idx == len(keywords) - 1 and smallest[-1] < shortest:
+                shortest = smallest[-1]
+                start, end = i - shortest + 1, i
+    return Subarray(start, end)
 ```
 
 ## Explanation
--- python_solutions/smallest_subarray_covering_all_values.py
+++ python_solutions/smallest_subarray_covering_all_values.py
@@ -5,12 +5,32 @@ from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
+
 Subarray = collections.namedtuple('Subarray', ('start', 'end'))
 
 
 def find_smallest_sequentially_covering_subset(paragraph, keywords):
-    # TODO - you fill in here.
-    return Subarray(0, 0)
+    key_lookup = {}
+    for i, s in enumerate(keywords):
+        key_lookup[s] = i
+    recent = [-1] * len(keywords)
+    smallest = [float('inf')] * len(keywords)
+    shortest = float('inf')
+    start, end = 0, 0
+
+    for i, s in enumerate(paragraph):
+        if s in key_lookup:
+            key_idx = key_lookup[s]
+            if key_idx == 0:
+                smallest[key_idx] = 1
+            elif smallest[key_idx - 1] != float('inf'):
+                prev_dist = i - recent[key_idx - 1]
+                smallest[key_idx] = prev_dist + smallest[key_idx - 1]
+            recent[key_idx] = i
+            if key_idx == len(keywords) - 1 and smallest[-1] < shortest:
+                shortest = smallest[-1]
+                start, end = i - shortest + 1, i
+    return Subarray(start, end)
 
 
 @enable_executor_hook
