0012326b59dfad8e6ef773accfbcecceacfbef29
jwl-7 <lucidrainbows@gmail.com>
2019-11-06 02:33:40 -0500

    Add recursive solution

--- python_explanations/string_decompositions_into_dictionary_words.md
+++ python_explanations/string_decompositions_into_dictionary_words.md
@@ -14,7 +14,25 @@ Output: [0, 3]
 
 ## Solution
 ```python
-BLANK
+def find_all_substrings(s, words):
+    def match_words(start):
+        curr_freq = collections.Counter()
+        for i in range(start, start + len(words) * word_len, word_len):
+            curr_word = s[i:i+word_len]
+            if word_freq[curr_word] == 0:
+                return False
+            curr_freq[curr_word] += 1
+            if curr_freq[curr_word] > word_freq[curr_word]:
+                return False
+        return True
+
+    word_len = len(words[0])
+    word_freq = collections.Counter(words)
+    result = []
+    for i in range(len(s) - word_len * len(words) + 1):
+        if match_words(i):
+            result.append(i)
+    return result
 ```
 
 ## Explanation
--- python_solutions/string_decompositions_into_dictionary_words.py
+++ python_solutions/string_decompositions_into_dictionary_words.py
@@ -1,9 +1,27 @@
+import collections
+
 from test_framework import generic_test
 
 
 def find_all_substrings(s, words):
-    # TODO - you fill in here.
-    return []
+    def match_words(start):
+        curr_freq = collections.Counter()
+        for i in range(start, start + len(words) * word_len, word_len):
+            curr_word = s[i:i+word_len]
+            if word_freq[curr_word] == 0:
+                return False
+            curr_freq[curr_word] += 1
+            if curr_freq[curr_word] > word_freq[curr_word]:
+                return False
+        return True
+
+    word_len = len(words[0])
+    word_freq = collections.Counter(words)
+    result = []
+    for i in range(len(s) - word_len * len(words) + 1):
+        if match_words(i):
+            result.append(i)
+    return result
 
 
 if __name__ == '__main__':
