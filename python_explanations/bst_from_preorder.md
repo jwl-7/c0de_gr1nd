ac2cc5ef3a65269d3a2b8da33ea9ee7240b9c214
jwl-7 <lucidrainbows@gmail.com>
2019-11-09 16:10:29 -0500

    Add solution

--- python_explanations/bst_from_preorder.md
+++ python_explanations/bst_from_preorder.md
@@ -15,7 +15,21 @@ Output:
 
 ## Solution
 ```python
-BLANK
+def rebuild_bst_from_preorder(preorder_sequence):
+    if not preorder_sequence:
+        return None
+
+    root = BinaryTreeNode(preorder_sequence[0])
+    i = 1
+    while i < len(preorder_sequence):
+        if preorder_sequence[i] < preorder_sequence[0]:
+            i += 1
+        else:
+            break
+
+    root.left = rebuild_bst_from_preorder(preorder_sequence[1:i])
+    root.right = rebuild_bst_from_preorder(preorder_sequence[i:])
+    return root
 ```
 
 ## Explanation
--- python_solutions/bst_from_preorder.py
+++ python_solutions/bst_from_preorder.py
@@ -1,9 +1,22 @@
 from test_framework import generic_test
+from binary_tree_node import BinaryTreeNode
 
 
 def rebuild_bst_from_preorder(preorder_sequence):
-    # TODO - you fill in here.
-    return None
+    if not preorder_sequence:
+        return None
+
+    root = BinaryTreeNode(preorder_sequence[0])
+    i = 1
+    while i < len(preorder_sequence):
+        if preorder_sequence[i] < preorder_sequence[0]:
+            i += 1
+        else:
+            break
+
+    root.left = rebuild_bst_from_preorder(preorder_sequence[1:i])
+    root.right = rebuild_bst_from_preorder(preorder_sequence[i:])
+    return root
 
 
 if __name__ == '__main__':
