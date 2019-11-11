be8f49944cbb74588ce5fd86f45dbb8030d0a191
jwl-7 <lucidrainbows@gmail.com>
2019-11-10 20:06:54 -0500

    Add solution

--- python_explanations/descendant_and_ancestor_in_bst.md
+++ python_explanations/descendant_and_ancestor_in_bst.md
@@ -26,7 +26,36 @@ Conclusion: 1 and 3 are descendants of 2
 
 ## Solution
 ```python
-BLANK
+def pair_includes_ancestor_and_descendant_of_m(node0, node1, middle):
+    a = node0
+    b = node1
+    while (
+        a is not node1 and
+        a is not middle and
+        b is not node0 and
+        b is not middle and
+        (a or b)
+    ):
+        if a:
+            a = a.left if a.data > middle.data else a.right
+        if b:
+            b = b.left if b.data > middle.data else b.right
+
+    if (
+        a is not middle and
+        b is not middle or
+        a is node1 or
+        b is node0
+    ):
+        return False
+
+    return search_bst(middle, node1 if a is middle else node0)
+
+
+def search_bst(tree, node):
+    while tree and tree is not node:
+        tree = tree.left if tree.data > node.data else tree.right
+    return tree is node
 ```
 
 ## Explanation
--- python_solutions/descendant_and_ancestor_in_bst.py
+++ python_solutions/descendant_and_ancestor_in_bst.py
@@ -6,8 +6,35 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def pair_includes_ancestor_and_descendant_of_m(node0, node1, middle):
-    # TODO - you fill in here.
-    return True
+    a = node0
+    b = node1
+    while (
+        a is not node1 and
+        a is not middle and
+        b is not node0 and
+        b is not middle and
+        (a or b)
+    ):
+        if a:
+            a = a.left if a.data > middle.data else a.right
+        if b:
+            b = b.left if b.data > middle.data else b.right
+
+    if (
+        a is not middle and
+        b is not middle or
+        a is node1 or
+        b is node0
+    ):
+        return False
+
+    return search_bst(middle, node1 if a is middle else node0)
+
+
+def search_bst(tree, node):
+    while tree and tree is not node:
+        tree = tree.left if tree.data > node.data else tree.right
+    return tree is node
 
 
 @enable_executor_hook
