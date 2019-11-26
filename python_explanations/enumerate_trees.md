888cfbc6e878051d880f8c1e0ef717c58af1ffa5
jwl-7 <lucidrainbows@gmail.com>
2019-11-26 00:05:24 -0500

    Add solution

--- python_explanations/enumerate_trees.md
+++ python_explanations/enumerate_trees.md
@@ -15,7 +15,21 @@ Output:
 
 ## Solution
 ```python
-BLANK
+def generate_all_binary_trees(num_nodes):
+    if not num_nodes:
+        return [None]
+
+    result = []
+    for num_left in range(num_nodes):
+        num_right = num_nodes - num_left - 1
+        left_tree = generate_all_binary_trees(num_left)
+        right_tree = generate_all_binary_trees(num_right)
+        result += [
+            BinaryTreeNode(0, left, right)
+            for left in left_tree
+            for right in right_tree
+        ]
+    return result
 ```
 
 ## Explanation
--- python_solutions/enumerate_trees.py
+++ python_solutions/enumerate_trees.py
@@ -2,11 +2,24 @@ import functools
 
 from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
+from binary_tree_node import BinaryTreeNode
 
 
 def generate_all_binary_trees(num_nodes):
-    # TODO - you fill in here.
-    return []
+    if not num_nodes:
+        return [None]
+
+    result = []
+    for num_left in range(num_nodes):
+        num_right = num_nodes - num_left - 1
+        left_tree = generate_all_binary_trees(num_left)
+        right_tree = generate_all_binary_trees(num_right)
+        result += [
+            BinaryTreeNode(0, left, right)
+            for left in left_tree
+            for right in right_tree
+        ]
+    return result
 
 
 def serialize_structure(tree):
