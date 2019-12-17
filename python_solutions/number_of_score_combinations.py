06f35bfb7dc51581f62a276dfe9b9b65263e2dcb
jwl-7 <lucidrainbows@gmail.com>
2019-12-17 02:38:43 -0500

    Rename table in code

--- python_solutions/number_of_score_combinations.py
+++ python_solutions/number_of_score_combinations.py
@@ -2,13 +2,13 @@ from test_framework import generic_test
 
 
 def num_combinations_for_final_score(final_score, play_scores):
-    combos = [[1] + [0] * final_score for _ in play_scores]
+    dp = [[1] + [0] * final_score for _ in play_scores]
     for i in range(1, final_score + 1):
         for j in range(len(play_scores)):
-            x = combos[j-1][i]
-            y = combos[j][i - play_scores[j]] if i >= play_scores[j] else 0
-            combos[j][i] = x + y
-    return combos[-1][-1]
+            x = dp[j-1][i]
+            y = dp[j][i - play_scores[j]] if i >= play_scores[j] else 0
+            dp[j][i] = x + y
+    return dp[-1][-1]
 
 
 if __name__ == '__main__':
