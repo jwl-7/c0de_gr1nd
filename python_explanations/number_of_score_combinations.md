952ab60eb683cbc0c5da97d69c26b86b3de9da22
jwl-7 <lucidrainbows@gmail.com>
2019-12-17 02:19:05 -0500

    Add solution

--- python_explanations/number_of_score_combinations.md
+++ python_explanations/number_of_score_combinations.md
@@ -19,7 +19,14 @@ Output: 3
 
 ## Solution
 ```python
-BLANK
+def num_combinations_for_final_score(final_score, play_scores):
+    combos = [[1] + [0] * final_score for _ in play_scores]
+    for i in range(1, final_score + 1):
+        for j in range(len(play_scores)):
+            x = combos[j-1][i]
+            y = combos[j][i - play_scores[j]] if i >= play_scores[j] else 0
+            combos[j][i] = x + y
+    return combos[-1][-1]
 ```
 
 ## Explanation
--- python_solutions/number_of_score_combinations.py
+++ python_solutions/number_of_score_combinations.py
@@ -1,9 +1,14 @@
 from test_framework import generic_test
 
 
-def num_combinations_for_final_score(final_score, individual_play_scores):
-    # TODO - you fill in here.
-    return 0
+def num_combinations_for_final_score(final_score, play_scores):
+    combos = [[1] + [0] * final_score for _ in play_scores]
+    for i in range(1, final_score + 1):
+        for j in range(len(play_scores)):
+            x = combos[j-1][i]
+            y = combos[j][i - play_scores[j]] if i >= play_scores[j] else 0
+            combos[j][i] = x + y
+    return combos[-1][-1]
 
 
 if __name__ == '__main__':
