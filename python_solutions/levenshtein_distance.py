1064fa75f293750175f967adaeb62001f28358ef
jwl-7 <lucidrainbows@gmail.com>
2019-12-17 05:19:05 -0500

    Add solution

--- python_solutions/levenshtein_distance.py
+++ python_solutions/levenshtein_distance.py
@@ -2,8 +2,18 @@ from test_framework import generic_test
 
 
 def levenshtein_distance(A, B):
-    # TODO - you fill in here.
-    return 0
+    dp = [[i] + [0] * len(B) for i in range(len(A) + 1)]
+    dp[0] = [j for j in range(len(B) + 1)]
+    for i in range(1, len(A) + 1):
+        for j in range(1, len(B) + 1):
+            insert = dp[i][j-1]
+            delete = dp[i-1][j]
+            replace = dp[i-1][j-1]
+            if A[i-1] == B[j-1]:
+                dp[i][j] = replace
+            else:
+                dp[i][j] = min(insert, delete, replace) + 1
+    return dp[-1][-1]
 
 
 if __name__ == '__main__':
