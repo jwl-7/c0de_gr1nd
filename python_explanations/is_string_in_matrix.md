9d7d8ca97c926452baf6cab1094274227f280d74
jwl-7 <lucidrainbows@gmail.com>
2019-12-18 02:09:44 -0500

    Add solution

--- python_explanations/is_string_in_matrix.md
+++ python_explanations/is_string_in_matrix.md
@@ -16,7 +16,32 @@ Output: False
 
 ## Solution
 ```python
-BLANK
+def is_pattern_contained_in_grid(grid, S):
+    def dfs(x, y, idx):
+        if idx == len(S):
+            return True
+
+        if not (
+            0 <= x < len(grid) and
+            0 <= y < len(grid[x]) and
+            grid[x][y] == S[idx] and
+            (x, y, idx) not in visited
+        ):
+            return False
+
+        for dx, dy in (-1, 0), (1, 0), (0, -1), (0, 1):
+            if dfs(x + dx, y + dy, idx + 1):
+                return True
+
+        visited.add((x, y, idx))
+        return False
+
+    visited = set()
+    return any(
+        dfs(i, j, 0)
+        for i in range(len(grid))
+        for j in range(len(grid[i]))
+    )
 ```
 
 ## Explanation
--- python_solutions/is_string_in_matrix.py
+++ python_solutions/is_string_in_matrix.py
@@ -2,8 +2,31 @@ from test_framework import generic_test
 
 
 def is_pattern_contained_in_grid(grid, S):
-    # TODO - you fill in here.
-    return True
+    def dfs(x, y, idx):
+        if idx == len(S):
+            return True
+
+        if not (
+            0 <= x < len(grid) and
+            0 <= y < len(grid[x]) and
+            grid[x][y] == S[idx] and
+            (x, y, idx) not in visited
+        ):
+            return False
+
+        for dx, dy in (-1, 0), (1, 0), (0, -1), (0, 1):
+            if dfs(x + dx, y + dy, idx + 1):
+                return True
+
+        visited.add((x, y, idx))
+        return False
+
+    visited = set()
+    return any(
+        dfs(i, j, 0)
+        for i in range(len(grid))
+        for j in range(len(grid[i]))
+    )
 
 
 if __name__ == '__main__':
