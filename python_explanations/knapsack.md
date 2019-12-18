b0314d6c16c9e22b745ab8e0d67ba9efbf705915
jwl-7 <lucidrainbows@gmail.com>
2019-12-18 04:05:44 -0500

    Simplify return statement

--- python_explanations/knapsack.md
+++ python_explanations/knapsack.md
@@ -14,7 +14,21 @@ Output: 35
 
 ## Solution
 ```python
-BLANK
+Item = collections.namedtuple('Item', ('weight', 'value'))
+
+
+def optimum_subject_to_capacity(items, capacity):
+    n = len(items)
+    dp = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        for j in range(1, capacity + 1):
+            weight = items[i-1].weight
+            value = items[i-1].value
+            if weight <= j:
+                dp[i][j] = max(value + dp[i-1][j-weight], dp[i-1][j])
+            else:
+                dp[i][j] = dp[i-1][j]
+    return dp[-1][-1]
 ```
 
 ## Explanation
--- python_solutions/knapsack.py
+++ python_solutions/knapsack.py
@@ -9,8 +9,17 @@ Item = collections.namedtuple('Item', ('weight', 'value'))
 
 
 def optimum_subject_to_capacity(items, capacity):
-    # TODO - you fill in here.
-    return 0
+    n = len(items)
+    dp = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        for j in range(1, capacity + 1):
+            weight = items[i-1].weight
+            value = items[i-1].value
+            if weight <= j:
+                dp[i][j] = max(value + dp[i-1][j-weight], dp[i-1][j])
+            else:
+                dp[i][j] = dp[i-1][j]
+    return dp[-1][-1]
 
 
 @enable_executor_hook
