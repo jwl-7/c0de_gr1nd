70182b28cd889161b2b16d30dcc9095a285acd7d
jwl-7 <lucidrainbows@gmail.com>
2019-12-19 03:48:02 -0500

    Add solution

--- python_explanations/minimum_weight_path_in_a_triangle.md
+++ python_explanations/minimum_weight_path_in_a_triangle.md
@@ -21,7 +21,13 @@ Output: 11
 
 ## Solution
 ```python
-BLANK
+def minimum_path_weight(triangle):
+    if not triangle:
+        return 0
+    for i in range(len(triangle) - 2, -1, -1):
+        for j in range(len(triangle[i])):
+            triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1])
+    return triangle[0][0]
 ```
 
 ## Explanation
--- python_solutions/minimum_weight_path_in_a_triangle.py
+++ python_solutions/minimum_weight_path_in_a_triangle.py
@@ -2,8 +2,12 @@ from test_framework import generic_test
 
 
 def minimum_path_weight(triangle):
-    # TODO - you fill in here.
-    return 0
+    if not triangle:
+        return 0
+    for i in range(len(triangle) - 2, -1, -1):
+        for j in range(len(triangle[i])):
+            triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1])
+    return triangle[0][0]
 
 
 if __name__ == '__main__':
