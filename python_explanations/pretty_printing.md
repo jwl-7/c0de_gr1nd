7bffcd6d25899c9fcf329e53593fd6d8dd8584ef
jwl-7 <lucidrainbows@gmail.com>
2020-01-10 01:52:11 -0500

    Add solution

--- python_explanations/pretty_printing.md
+++ python_explanations/pretty_printing.md
@@ -14,7 +14,20 @@ Output: 4
 
 ## Solution
 ```python
-BLANK
+def minimum_messiness(words, line_length):
+    char_count = line_length - len(words[0])
+    dp = [char_count**2] + [float('inf')] * (len(words) - 1)
+    for i in range(1, len(words)):
+        char_count = line_length - len(words[i])
+        dp[i] = dp[i-1] + char_count ** 2
+        for j in reversed(range(i)):
+            char_count -= len(words[j]) + 1
+            if char_count < 0:
+                break
+            first_j = 0 if j - 1 < 0 else dp[j-1]
+            curr_line = char_count ** 2
+            dp[i] = min(dp[i], first_j + curr_line)
+    return dp[-1]
 ```
 
 ## Explanation
--- python_solutions/pretty_printing.py
+++ python_solutions/pretty_printing.py
@@ -2,8 +2,19 @@ from test_framework import generic_test
 
 
 def minimum_messiness(words, line_length):
-    # TODO - you fill in here.
-    return 0
+    char_count = line_length - len(words[0])
+    dp = [char_count**2] + [float('inf')] * (len(words) - 1)
+    for i in range(1, len(words)):
+        char_count = line_length - len(words[i])
+        dp[i] = dp[i-1] + char_count ** 2
+        for j in reversed(range(i)):
+            char_count -= len(words[j]) + 1
+            if char_count < 0:
+                break
+            first_j = 0 if j - 1 < 0 else dp[j-1]
+            curr_line = char_count ** 2
+            dp[i] = min(dp[i], first_j + curr_line)
+    return dp[-1]
 
 
 if __name__ == '__main__':
