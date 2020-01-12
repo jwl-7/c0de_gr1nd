a69e261c25e2be6e45c137aff3e5f1fbe39c2591
jwl-7 <lucidrainbows@gmail.com>
2020-01-12 18:52:17 -0500

    Add solution

--- python_solutions/two_sum.py
+++ python_solutions/two_sum.py
@@ -2,8 +2,16 @@ from test_framework import generic_test
 
 
 def has_two_sum(A, t):
-    # TODO - you fill in here.
-    return True
+    i = 0
+    j = len(A) - 1
+    while i <= j:
+        if A[i] + A[j] == t:
+            return True
+        elif A[i] + A[j] < t:
+            i += 1
+        else:
+            j -= 1
+    return False
 
 
 if __name__ == '__main__':
