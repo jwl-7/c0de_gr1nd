bdf83a98ca78267cc62732cb26710d7a788d9d03
jwl-7 <lucidrainbows@gmail.com>
2020-01-13 23:33:49 -0500

    Add solution

--- python_explanations/matrix_enclosed_regions.md
+++ python_explanations/matrix_enclosed_regions.md
@@ -18,7 +18,16 @@ Output: [['B', 'B', 'B', 'B'],
 
 ## Solution
 ```python
-BLANK
+def fill_surrounded_regions(board):
+    m = len(board)
+    n = len(board[0])
+    keep = [ab for k in range(m + n) for ab in ((0, k), (m - 1, k), (k, 0), (k, n - 1))]
+    while keep:
+        a, b = keep.pop()
+        if 0 <= a < m and 0 <= b < n and board[a][b] == 'W':
+            board[a][b] = 'X'
+            keep += (a, b - 1), (a, b + 1), (a - 1, b), (a + 1, b)
+    board[:] = [['B' if char != 'X' else 'W' for char in row] for row in board]
 ```
 
 ## Explanation
--- python_solutions/matrix_enclosed_regions.py
+++ python_solutions/matrix_enclosed_regions.py
@@ -2,8 +2,15 @@ from test_framework import generic_test
 
 
 def fill_surrounded_regions(board):
-    # TODO - you fill in here.
-    return
+    m = len(board)
+    n = len(board[0])
+    keep = [ab for k in range(m + n) for ab in ((0, k), (m - 1, k), (k, 0), (k, n - 1))]
+    while keep:
+        a, b = keep.pop()
+        if 0 <= a < m and 0 <= b < n and board[a][b] == 'W':
+            board[a][b] = 'X'
+            keep += (a, b - 1), (a, b + 1), (a - 1, b), (a + 1, b)
+    board[:] = [['B' if char != 'X' else 'W' for char in row] for row in board]
 
 
 def fill_surrounded_regions_wrapper(board):
