b18123e19de02a826db6d49b144913249be9173f
jwl-7 <lucidrainbows@gmail.com>
2020-01-14 21:59:59 -0500

    Add solution

--- python_explanations/string_transformability.md
+++ python_explanations/string_transformability.md
@@ -24,7 +24,20 @@ Output: -1
 
 ## Solution
 ```python
-BLANK
+def transform_string(D, s, t):
+    q = collections.deque([(s, 0)])
+    visited = set()
+    while q:
+        word, dist = q.popleft()
+        if word == t:
+            return dist
+        for i in range(len(word)):
+            for j in string.ascii_lowercase:
+                nxt_word = word[:i] + j + word[i+1:]
+                if nxt_word not in visited and nxt_word in D:
+                    q.append((nxt_word, dist + 1))
+                    visited.add(nxt_word)
+    return -1
 ```
 
 ## Explanation
--- python_solutions/string_transformability.py
+++ python_solutions/string_transformability.py
@@ -1,10 +1,23 @@
+import collections
+import string
+
 from test_framework import generic_test
 
 
-# Uses BFS to find the least steps of transformation.
 def transform_string(D, s, t):
-    # TODO - you fill in here.
-    return 0
+    q = collections.deque([(s, 0)])
+    visited = set()
+    while q:
+        word, dist = q.popleft()
+        if word == t:
+            return dist
+        for i in range(len(word)):
+            for j in string.ascii_lowercase:
+                nxt_word = word[:i] + j + word[i+1:]
+                if nxt_word not in visited and nxt_word in D:
+                    q.append((nxt_word, dist + 1))
+                    visited.add(nxt_word)
+    return -1
 
 
 if __name__ == '__main__':
